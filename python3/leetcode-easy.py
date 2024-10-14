69841b21550a96196829b8091008941333185156
Andrew Nguyen <155884683+tuanlong1106@users.noreply.github.com>
2024-10-14 10:56:44 +0700

    Update leetcode-easy.py
    Problem 21, 26

--- python3/leetcode-easy.py
+++ python3/leetcode-easy.py
@@ -64,7 +64,7 @@ class Solution:
     C can be placed before D (500) and M (1000) to make 400 and 900.
     Given a roman numeral, convert it to an integer.
 
-    Example 1:
+    Example :
     Input: s = "III"
     Output: 3
     Explanation: III = 3.
@@ -140,3 +140,64 @@ class Solution:
         if(len(sym)==0):
             return True
         return False
+    
+    """
+    21. Merge Two Sorted Lists
+    You are given the heads of two sorted linked lists list1 and list2.
+    Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+    Return the head of the merged linked list.
+    
+    Example:
+    Input: list1 = [1,2,4], list2 = [1,3,4]
+    Output: [1,1,2,3,4,4]
+    """
+    # Definition for singly-linked list.
+    # class ListNode:
+    #     def __init__(self, val=0, next=None):
+    #         self.val = val
+    #         self.next = next
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        cur = dummy = ListNode()
+        while list1 and list2:
+            if list1.val > list2.val:
+                cur.next = list2
+                list2 = list2.next
+            else: 
+                cur.next = list1
+                list1 = list1.next
+            cur = cur.next
+        if list1:
+            cur.next = list1
+        else: 
+            cur.next = list2
+        return dummy.next
+    
+    """
+    26. Remove Duplicates from Sorted Array
+    Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+    Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+    Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+    Return k.
+    Custom Judge:
+    The judge will test your solution with the following code:
+    int[] nums = [...]; // Input array
+    int[] expectedNums = [...]; // The expected answer with correct length
+    int k = removeDuplicates(nums); // Calls your implementation
+    assert k == expectedNums.length;
+    for (int i = 0; i < k; i++) {
+        assert nums[i] == expectedNums[i];
+    }
+    If all assertions pass, then your solution will be accepted.
+    Example:
+    Input: nums = [1,1,2]
+    Output: 2, nums = [1,2,_]
+    Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+    It does not matter what you leave beyond the returned k (hence they are underscores).
+    """
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1,len(nums)):
+            if nums[i] !=  nums[i-1]:
+                nums[j] = nums[i]
+                j += 1 
+        return j
