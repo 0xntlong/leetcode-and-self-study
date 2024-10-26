85aa9676c190ae1ff8daba13710f4be59b9091c2
Andrew Nguyen <155884683+tuanlong1106@users.noreply.github.com>
2024-10-26 15:53:34 +0700

    Create 1413.py

---
+++ python3/easy-solution/1413.py
@@ -0,0 +1,31 @@
+"""
+1413. Minimum Value to Get Positive Step by Step Sum
+    Given an array of integers nums, you start with an initial positive value startValue.
+    In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
+    Return the minimum positive value of startValue such that the step by step sum is never less than 1.
+
+    Example :
+    Input: nums = [-3,2,-3,4,2]
+    Output: 5
+    Explanation: If you choose startValue = 4, in the third iteration your step by step sum is less than 1.
+    step by step sum
+    startValue = 4 | startValue = 5 | nums
+    (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+    (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+    (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+    (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+    (4 +2 ) = 6  | (5 +2 ) = 7    |   2
+"""
+
+from typing import List
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        Sum, res = 0, 0
+        for i in range(len(nums)):
+            Sum += nums[i]
+            if Sum < 1:
+                res += (1 - Sum)
+                Sum = 1
+        if res == 0:
+            res = 1
+        return res
