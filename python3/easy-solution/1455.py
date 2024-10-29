52d69faf7209635759720bfa43e0350fa3632b18
Andrew Nguyen <155884683+tuanlong1106@users.noreply.github.com>
2024-10-29 16:07:54 +0700

    Create 1455.py

---
+++ python3/easy-solution/1455.py
@@ -0,0 +1,27 @@
+"""
+1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
+    Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
+    Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+    A prefix of a string s is any leading contiguous substring of s.
+
+    Example :
+    Input: sentence = "i love eating burger", searchWord = "burg"
+    Output: 4
+    Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+"""
+
+
+class Solution:
+    def fact(self, s: str, t: str) ->bool:
+        if len(t) > len(s):
+            return False
+        for i in range(len(t)):
+            if s[i] != t[i]:
+                return False
+        return True
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split()
+        for idx , word in enumerate(words, start = 1):
+            if self.fact(word, searchWord):
+                return idx
+        return -1
