bb117fa1bb7d06c7efdb052a5308fe142aa90f8a
Andrew Nguyen <155884683+tuanlong1106@users.noreply.github.com>
2024-11-13 15:41:00 +0700

    Create 2563.cc

---
+++ cpp/medium-solution/2563.cc
@@ -0,0 +1,30 @@
+/*
+2563. Count the Number of Fair Pairs
+    Given a 0-indexed integer array nums of size n and two integers lower and upper, return the number of fair pairs.
+    A pair (i, j) is fair if:
+    0 <= i < j < n, and
+    lower <= nums[i] + nums[j] <= upper
+    
+    Example :
+    Input: nums = [0,1,7,4,4,5], lower = 3, upper = 6
+    Output: 6
+    Explanation: There are 6 fair pairs: (0,3), (0,4), (0,5), (1,3), (1,4), and (1,5).
+*/
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        std::sort(nums.begin(), nums.end());
+        long long total = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++){
+            auto left = std::lower_bound(nums.begin(), nums.begin() + i, lower - nums[i]);
+            auto right = std::upper_bound(nums.begin(), nums.begin() + i, upper - nums[i]);
+            total += (right - left);
+        }
+        return total;
+    }
+};
