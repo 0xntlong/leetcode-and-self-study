08f8ee24d9e1eaf6bf83b119616783cea4b0d47b
Andrew Nguyen <155884683+tuanlong1106@users.noreply.github.com>
2024-11-19 15:59:28 +0700

    Create 1716.py

---
+++ python3/easy-solution/1716.py
@@ -0,0 +1,24 @@
+"""
+1716. Calculate Money in Leetcode Bank
+    Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+    He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+    Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+    Example :
+    Input: n = 4
+    Output: 10
+    Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+"""
+
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        res = 0
+        monday = 1
+        while n > 0:
+            money = monday
+            for i in range(min(n, 7)):
+                res += money
+                money += 1
+            n -= 7
+            monday += 1
+        return res
