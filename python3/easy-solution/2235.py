f9d1b396275bd3c8f32406bc1e8299b0f72a4053
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2024-12-26 16:36:22 +0700

    Create 2235.py

---
+++ python3/easy-solution/2235.py
@@ -0,0 +1,13 @@
+"""
+2235. Add Two Intergers
+    Given two integers num1 and num2, return the sum of the two integers.
+
+    Example :
+    Input: num1 = 12, num2 = 5
+    Output: 17
+    Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+"""
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return (num1 + num2)
