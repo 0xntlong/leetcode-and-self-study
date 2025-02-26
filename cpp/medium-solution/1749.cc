162a7ec69d724a810094bd8383eaa1ed5849e02a
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-02-27 00:16:56 +0700

    Update 1749.cc

--- cpp/medium-solution/1749.cc
+++ cpp/medium-solution/1749.cc
@@ -6,7 +6,7 @@
     If x is a negative integer, then abs(x) = -x.
     If x is a non-negative integer, then abs(x) = x.
     
-    Example 1:
+    Example:
     Input: nums = [1,-3,2,3,-4]
     Output: 5
     Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
