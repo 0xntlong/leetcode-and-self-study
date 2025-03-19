800ed7b3619f36e5b5a9bada7aa6139b3e2dec05
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-03-19 16:06:16 +0700

    Create 2643.cc

---
+++ cpp/easy-solution/2643.cc
@@ -0,0 +1,30 @@
+/*
+2643. Row with Maximum Ones
+    Given a m x n binary matrix mat, find the 0-indexed position of the row that contains the maximum count of ones, and the number of ones in that row.
+    In case there are multiple rows that have the maximum count of ones, the row with the smallest row number should be selected.
+    Return an array containing the index of the row, and the number of ones in it.
+
+    Example :
+    Input: mat = [[0,1],[1,0]]
+    Output: [0,1]
+    Explanation: Both rows have the same number of 1's. So we return the index of the smaller row, 0, and the maximum count of ones (1). So, the answer is [0,1]. 
+*/
+
+#include <vector>
+#include <numeric>
+using namespace std;
+class Solution {
+    public:
+        vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+            std::vector<int> res = {0, 0};
+            int row = mat.size();
+            for (int i = 0; i < row; i++){
+                int count = std::accumulate(mat[i].begin(), mat[i].end(), 0);
+                if (count > res[1]){
+                    res[0] = i;
+                    res[1] = count;
+                }
+            }
+            return res;
+        }
+    };
