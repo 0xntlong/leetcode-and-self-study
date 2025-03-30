f8957691f7e23753fd7a4e953c0f2a390351f5bf
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-03-30 10:21:37 +0700

    Create 763.js

---
+++ JS/medium-solution/763.js
@@ -0,0 +1,36 @@
+/**
+763. Partition Labels
+    You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part. For example, the string "ababcc" can be partitioned into ["abab", "cc"], but partitions such as ["aba", "bcc"] or ["ab", "ab", "cc"] are invalid.
+    Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+    Return a list of integers representing the size of these parts.
+    Example :
+    Input: s = "ababcbacadefegdehijhklij"
+    Output: [9,7,8]
+    Explanation:
+    The partition is "ababcbaca", "defegde", "hijhklij".
+    This is a partition so that each letter appears in at most one part.
+    A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+ */
+
+
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var partitionLabels = function(s) {
+    const n = s.length;
+    const alphabet = new Array(26).fill(-1);
+    for (let i = 0; i < n; i++){
+        alphabet[s.charCodeAt(i) - 97] = i;
+    }
+    const res = [];
+    let start = 0, end = -1;
+    for (let i = 0; i < n; i++){
+        end = Math.max(end, alphabet[s.charCodeAt(i) - 97]);
+        if (i === end){
+            res.push(end - start + 1);
+            start = i + 1;
+        }
+    }
+    return res;
+};
