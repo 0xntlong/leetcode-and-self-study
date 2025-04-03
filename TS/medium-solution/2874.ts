12d8b57e09c36abe6ab4047bf4a1ee2a593c82f9
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-04-03 15:20:17 +0700

    Create 2874.ts

---
+++ TS/medium-solution/2874.ts
@@ -0,0 +1,24 @@
+/**
+2874. Maximum Value of an Ordered Triplet II
+    You are given a 0-indexed integer array nums.
+    Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+    The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+
+    Example :
+    Input: nums = [12,6,1,2,7]
+    Output: 77
+    Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+    It can be shown that there are no ordered triplets of indices with a value greater than 77. 
+ */
+
+    function maximumTripletValue(nums: number[]): number {
+        let highest: number = 0;
+        let diff: number = 0;
+        let res: number = 0;
+        for (let num of nums){
+            res = Math.max(res, diff * num);
+            diff = Math.max(diff, highest - num);
+            highest = Math.max(highest, num);
+        }
+        return res;
+    };
