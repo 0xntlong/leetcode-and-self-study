f1d950dd1687c395d8fd7e8dd990e16a60ad932f
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-04-13 16:23:15 +0700

    Create 1922.ts

---
+++ TS/medium-solution/1922.ts
@@ -0,0 +1,34 @@
+/**
+1922. Count Good NUmbers
+    A digit string is good if the digits (0-indexed) at even indices are even and the digits at odd indices are prime (2, 3, 5, or 7).
+    For example, "2582" is good because the digits (2 and 8) at even positions are even and the digits (5 and 2) at odd positions are prime. However, "3245" is not good because 3 is at an even index but is not even.
+    Given an integer n, return the total number of good digit strings of length n. Since the answer may be large, return it modulo 109 + 7.
+    A digit string is a string consisting of digits 0 through 9 that may contain leading zeros.
+
+    Example :
+    Input: n = 1
+    Output: 5
+    Explanation: The good numbers of length 1 are "0", "2", "4", "6", "8".
+ */
+
+
+    function countGoodNumbers(n: number): number {
+        const MOD: bigint = BigInt(10 ** 9 + 7);
+    
+        function powMOD(x: bigint, num: bigint): bigint {
+            if (num === 0n) {
+                return 1n;
+            } else if (num % 2n === 0n) {
+                return powMOD((x * x) % MOD, num / 2n);
+            }
+            return (x * powMOD(x, num - 1n)) % MOD;
+        }
+    
+        const nBig: bigint = BigInt(n);
+        const result: bigint = (
+            powMOD(5n, nBig % 2n) * 
+            powMOD(20n, nBig / 2n)
+        ) % MOD;
+    
+        return Number(result);
+    }
