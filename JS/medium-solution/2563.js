2f5fd51dcf947edb6f6d1f1c57010f42e06802b2
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-04-19 13:42:02 +0700

    Create 2563.js

---
+++ JS/medium-solution/2563.js
@@ -0,0 +1,37 @@
+/**
+2563. Count the Number of Fair Pairs
+    Given a 0-indexed integer array nums of size n and two integers lower and upper, return the number of fair pairs.
+    A pair (i, j) is fair if:
+    0 <= i < j < n, and
+    lower <= nums[i] + nums[j] <= upper
+    
+    Example :
+    Input: nums = [0,1,7,4,4,5], lower = 3, upper = 6
+    Output: 6
+    Explanation: There are 6 fair pairs: (0,3), (0,4), (0,5), (1,3), (1,4), and (1,5).
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {number}
+ */
+var countFairPairs = function(nums, lower, upper) {
+    nums.sort((a, b) => a - b);
+    const cnt = (nums, x) => {
+        let left = 0, right = nums.length - 1;
+        let ans = 0;
+        while (left < right){
+            let sum = nums[left] + nums[right];
+            if (sum < x){
+                ans += (right - left);
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    };
+    return cnt(nums, upper + 1) - cnt(nums, lower);
+};
