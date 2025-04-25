0728d8183e2b340842bf74bc8f924a87620a8443
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-04-25 16:58:43 +0700

    Create 2845.js

---
+++ JS/medium-solution/2845.js
@@ -0,0 +1,45 @@
+/**
+2845. Count of Interesting Subarrays
+    You are given a 0-indexed integer array nums, an integer modulo, and an integer k.
+    Your task is to find the count of subarrays that are interesting.
+    A subarray nums[l..r] is interesting if the following condition holds:
+    Let cnt be the number of indices i in the range [l, r] such that nums[i] % modulo == k. Then, cnt % modulo == k.
+    Return an integer denoting the count of interesting subarrays.
+    Note: A subarray is a contiguous non-empty sequence of elements within an array.
+
+    Example :
+    Input: nums = [3,2,4], modulo = 2, k = 1
+    Output: 3
+    Explanation: In this example the interesting subarrays are: 
+    The subarray nums[0..0] which is [3]. 
+    - There is only one index, i = 0, in the range [0, 0] that satisfies nums[i] % modulo == k. 
+    - Hence, cnt = 1 and cnt % modulo == k.  
+    The subarray nums[0..1] which is [3,2].
+    - There is only one index, i = 0, in the range [0, 1] that satisfies nums[i] % modulo == k.  
+    - Hence, cnt = 1 and cnt % modulo == k.
+    The subarray nums[0..2] which is [3,2,4]. 
+    - There is only one index, i = 0, in the range [0, 2] that satisfies nums[i] % modulo == k. 
+    - Hence, cnt = 1 and cnt % modulo == k. 
+    It can be shown that there are no other interesting subarrays. So, the answer is 3.
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} modulo
+ * @param {number} k
+ * @return {number}
+ */
+var countInterestingSubarrays = function(nums, modulo, k) {
+    const n = nums.length;
+    const hashmap = new Map();
+    hashmap.set(0, 1);
+    let res = 0;
+    let prefix = 0;
+    for (let i = 0; i < n; i++){
+        prefix += (nums[i] % modulo === k) ? 1 : 0;
+        res += (hashmap.get((prefix + modulo - k) % modulo) || 0);
+        hashmap.set(prefix % modulo, (hashmap.get(prefix % modulo) || 0) + 1);
+    }
+    return res;
+};
