cebc76661f03608641411a3ae2f0d486626269d1
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-05-11 16:52:36 +0700

    Create 1550.ts

---
+++ TS/easy-solution/1550.ts
@@ -0,0 +1,21 @@
+/**
+1550. Three Consecutive Odds
+    Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+    Example :
+    Input: arr = [2,6,4,1]
+    Output: false
+    Explanation: There are no three consecutive odds.
+ */
+
+
+function threeConsecutiveOdds(arr: number[]): boolean {
+    if (arr.length < 3){
+        return false;
+    }
+    for (let i = 0; i < arr.length - 2; i++){
+        if (arr[i] % 2 !== 0 && arr[i +1] % 2 !== 0 && arr[i + 2] % 2 !== 0){
+            return true;
+        }
+    }
+    return false;
+};
