616b7b010556c0d9bce2392f78e19c5aeab37eee
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-05-16 13:10:36 +0700

    Create 2901.ts

---
+++ TS/medium-solution/2901.ts
@@ -0,0 +1,65 @@
+/**
+2901. Longest Unequal Adjacent Groups Subsequence II
+    You are given a string array words, and an array groups, both arrays having length n.
+    The hamming distance between two strings of equal length is the number of positions at which the corresponding characters are different.
+    You need to select the longest subsequence from an array of indices [0, 1, ..., n - 1], such that for the subsequence denoted as [i0, i1, ..., ik-1] having length k, the following holds:
+    For adjacent indices in the subsequence, their corresponding groups are unequal, i.e., groups[ij] != groups[ij+1], for each j where 0 < j + 1 < k.
+    words[ij] and words[ij+1] are equal in length, and the hamming distance between them is 1, where 0 < j + 1 < k, for all indices in the subsequence.
+    Return a string array containing the words corresponding to the indices (in order) in the selected subsequence. If there are multiple answers, return any of them.
+    Note: strings in words may be unequal in length.
+
+    Example 1:
+    Input: words = ["bab","dab","cab"], groups = [1,2,2]
+    Output: ["bab","cab"]
+    Explanation: A subsequence that can be selected is [0,2].
+    groups[0] != groups[2]
+    words[0].length == words[2].length, and the hamming distance between them is 1.
+    So, a valid answer is [words[0],words[2]] = ["bab","cab"].
+    Another subsequence that can be selected is [0,1].
+    groups[0] != groups[1]
+    words[0].length == words[1].length, and the hamming distance between them is 1.
+    So, another valid answer is [words[0],words[1]] = ["bab","dab"].
+    It can be shown that the length of the longest subsequence of indices that satisfies the conditions is 2.
+ */
+
+    function getWordsInLongestSubsequence(words: string[], groups: number[]): string[] {
+    function hamming(s : string, t : string): boolean{
+        if (s.length !== t.length){
+            return false;
+        }
+        let diff = 0;
+        for (let i = 0; i < s.length; i++){
+            if (s[i] !== t[i]){
+                diff++;
+                if (diff > 1){
+                    return false;
+                }
+            }
+        }
+        return diff === 1;
+    }
+    const n = words.length;
+    const dp = new Array<number>(n).fill(1);
+    const nxt = new Array<number>(n).fill(-1);
+    let max_len = 0, start = -1;
+    for (let i = n - 1; i >= 0; i--){
+        for (let j = i + 1; j < n; j++){
+            if (groups[i] !== groups[j] && hamming(words[i], words[j])){
+                if (dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                    nxt[i] = j;
+                }
+            }
+        }
+        if (dp[i] > max_len){
+            max_len = dp[i];
+            start = i;
+        }
+    }
+    const res : string[] = [];
+    while (start !== -1){
+        res.push(words[start]);
+        start = nxt[start];
+    }
+    return res;
+};
