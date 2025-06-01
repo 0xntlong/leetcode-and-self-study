ceaaa5b44ef7e58bb6d2cc5c4d02c567f99d2e16
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-06-01 13:12:39 +0700

    Create 2929.js

---
+++ JS/medium-solution/2929.js
@@ -0,0 +1,29 @@
+/**
+2929. Distribute Candies Among Children II
+    You are given two positive integers n and limit.
+    Return the total number of ways to distribute n candies among 3 children such that no child gets more than limit candies.
+
+    Example :
+    Input: n = 5, limit = 2
+    Output: 3
+    Explanation: There are 3 ways to distribute 5 candies such that no child gets more than 2 candies: (1, 2, 2), (2, 1, 2) and (2, 2, 1).
+ */
+
+/**
+ * @param {number} n
+ * @param {number} limit
+ * @return {number}
+ */
+
+function C(n){
+    if (n < 0)
+        return 0;
+    return (n + 2) * (n + 1) / 2;
+}
+
+var distributeCandies = function(n, limit) {
+    return C(n)
+        - 3 * C(n - limit - 1)
+        + 3 * C(n - 2 * (limit + 1))
+        - C(n - 3 * (limit + 1));
+};
