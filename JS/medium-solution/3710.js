1008f5ce9d34def194d3e5aa57cfb67b5e791c6e
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-06-07 17:06:09 +0700

    Create 3710.js

---
+++ JS/medium-solution/3710.js
@@ -0,0 +1,50 @@
+/**
+3170. Lexicographically Minimum String After Removing Stars
+    You are given a string s. It may contain any number of '*' characters. Your task is to remove all '*' characters.
+    While there is a '*', do the following operation:
+    Delete the leftmost '*' and the smallest non-'*' character to its left. If there are several smallest characters, you can delete any of them.
+    Return the lexicographically smallest resulting string after removing all '*' characters.
+
+    Example :
+    Input: s = "aaba*"
+    Output: "aab"
+    Explanation:
+    We should delete one of the 'a' characters with '*'. If we choose s[3], s becomes the lexicographically smallest.
+ */
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var clearStars = function(s) {
+    let n = s.length;
+    let alpha = Array.from({length : 26}, () => []);
+    let s0 = 0;
+    s = s.split('');
+    for (let i = 0; i < n; i++){
+        let c = s[i];
+        if (c !== '*'){
+            alpha[c.charCodeAt(0) - 'a'.charCodeAt(0)].push(s0);
+            s[s0] = c;
+            s0++;
+        } else {
+            for (let x = 0; x < 26; x++){
+                if (alpha[x].length > 0){
+                    let k = alpha[x].pop();
+                    s[k] = '';
+                    break;
+                }
+            }
+        }
+    }
+    let j = 0; 
+    for (let i = 0; i < s0; i++){
+        if (s[i] >= 'a'){
+            s[j] = s[i];
+            j++;
+        }
+    }
+    s = s.slice(0, j).join('');
+    return s;
+};
