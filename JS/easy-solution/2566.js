c3657731c3c4feb355772c23e6cf4a8fd0e0b014
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-06-14 15:25:11 +0700

    Create 2566.js

---
+++ JS/easy-solution/2566.js
@@ -0,0 +1,55 @@
+/**
+2566. Maximum Difference by Remapping a Digit
+    You are given an integer num. You know that Bob will sneakily remap one of the 10 possible digits (0 to 9) to another digit.
+    Return the difference between the maximum and minimum values Bob can make by remapping exactly one digit in num.
+    Notes:
+    When Bob remaps a digit d1 to another digit d2, Bob replaces all occurrences of d1 in num with d2.
+    Bob can remap a digit to itself, in which case num does not change.
+    Bob can remap different digits for obtaining minimum and maximum values respectively.
+    The resulting number after remapping can contain leading zeroes.
+
+    Example:
+    Input: num = 11891
+    Output: 99009
+    Explanation: 
+    To achieve the maximum value, Bob can remap the digit 1 to the digit 9 to yield 99899.
+    To achieve the minimum value, Bob can remap the digit 1 to the digit 0, yielding 890.
+    The difference between these two numbers is 99009.
+*/
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minMaxDifference = function(num) {
+    const numStr = num.toString();
+    let maxArr = numStr.split('');
+    for (let i = 0; i < maxArr.length; i++){
+        if (maxArr[i] !== '9'){
+            const target = maxArr[i];
+            for (let j = 0; j < maxArr.length; j++){
+                if (maxArr[j] === target){
+                    maxArr[j] = '9';
+                }
+            }
+            break;
+        }
+    }
+    const maxNum = parseInt(maxArr.join(''));
+
+    let minArr = numStr.split('');
+    for (let i = 0; i < minArr.length; i++){
+        if (minArr[i] !== '0'){
+            const target = minArr[i];
+            for (let j = 0; j < minArr.length; j++){
+                if (minArr[j] === target){
+                    minArr[j] = '0';
+                }
+            }
+            break;
+        }
+    }
+    const minNum = parseInt(minArr.join(''));
+    return maxNum - minNum;
+};
