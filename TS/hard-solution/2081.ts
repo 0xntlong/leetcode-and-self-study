9e0c668f6ed0d36d7c0bdeab200ed2e21b2ec6a5
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-06-23 16:06:56 +0700

    Create 2081.ts

---
+++ TS/hard-solution/2081.ts
@@ -0,0 +1,69 @@
+/**
+2081. Sum of k-Mirror Numbers
+    A k-mirror number is a positive integer without leading zeros that reads the same both forward and backward in base-10 as well as in base-k.
+    For example, 9 is a 2-mirror number. The representation of 9 in base-10 and base-2 are 9 and 1001 respectively, which read the same both forward and backward.
+    On the contrary, 4 is not a 2-mirror number. The representation of 4 in base-2 is 100, which does not read the same both forward and backward.
+    Given the base k and the number n, return the sum of the n smallest k-mirror numbers.
+
+    Example :
+    Input: k = 2, n = 5
+    Output: 25
+    Explanation:
+    The 5 smallest 2-mirror numbers and their representations in base-2 are listed as follows:
+    base-10    base-2
+        1          1
+        3          11
+        5          101
+        7          111
+        9          1001
+    Their sum = 1 + 3 + 5 + 7 + 9 = 25.
+ */
+
+
+
+function reverse(x: number): number {
+    let res = 0;
+    while (x > 0) {
+        res = res * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    return res;
+}
+
+function Palindrome(x: number, k: number): boolean {
+    const digits: number[] = [];
+    while (x > 0) {
+        digits.push(x % k);
+        x = Math.floor(x / k);
+    }
+    for (let i = 0, j = digits.length - 1; i < j; i++, j--) {
+        if (digits[i] !== digits[j]) return false;
+    }
+    return true;
+}
+
+function isPalindrome(mid: number, odd: boolean): number {
+    const len: number = String(mid).length;
+    const rev: number = reverse(odd ? Math.floor(mid / 10) : mid);
+    const fac: number = 10 ** (odd ? len - 1 : len);
+    return mid * fac + rev;
+}
+
+function kMirror(k: number, n: number): number {
+    let total = 0;
+    let length = 1;
+    while (true) {
+        const mid = Math.floor((length + 1) / 2);
+        const start = 10 ** (mid - 1);
+        const end = 10 ** mid;
+        for (let i = start; i < end; i++) {
+            const pal: number = isPalindrome(i, length % 2 === 1);
+            if (Palindrome(pal, k)) {
+                total += pal;
+                n--;
+                if (n === 0) return total;
+            }
+        }
+        length++;
+    }
+}
