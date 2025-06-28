517e8a55b41f6f2fa936bd5df0ffba399019919d
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-06-28 15:37:23 +0700

    Create 2099.ts

---
+++ TS/easy-solution/2099.ts
@@ -0,0 +1,29 @@
+/**
+2099. Find Subsequence of Length K With the Largest Sum
+    You are given an integer array nums and an integer k. You want to find a subsequence of nums of length k that has the largest sum.
+    Return any such subsequence as an integer array of length k.
+    A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+    Example :
+    Input: nums = [2,1,3,3], k = 2
+    Output: [3,3]
+    Explanation:
+    The subsequence has the largest sum of 3 + 3 = 6.
+ */
+
+
+
+function maxSubsequence(nums: number[], k: number): number[] {
+    const n = nums.length;
+    if (n == k){
+        return nums;
+    }
+    let res: [number, number][] = [];
+    for (let i = 0; i < nums.length; i++){
+        res.push([nums[i], i]);
+    }
+    res.sort((a, b) => b[0] - a[0]);
+    let minHeap = res.slice(0, k);
+    minHeap.sort((a, b) => a[1] - b[1]);
+    return minHeap.map((a) => a[0]);
+};
