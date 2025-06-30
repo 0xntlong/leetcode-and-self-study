012a3c618ab79de514a2e915dac512f27de1bfd9
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-06-30 15:13:14 +0700

    Create 594.ts

---
+++ TS/easy-solution/594.ts
@@ -0,0 +1,27 @@
+/**
+594. Longest Harmonious Subsequence
+    We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+    Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+
+    Example :
+    Input: nums = [1,3,2,2,5,2,3,7]
+    Output: 5
+    Explanation:
+    The longest harmonious subsequence is [3,2,2,2,3].
+*/
+
+
+
+function findLHS(nums: number[]): number {
+    const freq = new Map();
+    for (const num of nums){
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+    let res = 0;
+    for (const [key, val] of freq.entries()){
+        if (freq.has(key + 1)){
+            res = Math.max(res, val + freq.get(key + 1));
+        }
+    }
+    return res;
+};
