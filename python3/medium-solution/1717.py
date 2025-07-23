159d98b2c52981f8abf4f995c9d1678a226322c9
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-07-23 15:32:57 +0700

    Create 1717.py

---
+++ python3/medium-solution/1717.py
@@ -0,0 +1,47 @@
+"""
+1717. Maximum Score From Removing Substrings
+    You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+    Remove substring "ab" and gain x points.
+    For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+    Remove substring "ba" and gain y points.
+    For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+    Return the maximum points you can gain after applying the above operations on s.
+
+    Example :
+    Input: s = "cdbcbbaaabab", x = 4, y = 5
+    Output: 19
+    Explanation:
+    - Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+    - Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+    - Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+    - Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+    Total score = 5 + 4 + 5 + 5 = 19.
+    
+"""
+
+
+
+
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        if x > y:
+            return self.getMax(s, 'a', 'b', x, y)
+        else:
+            return self.getMax(s, 'b', 'a', y, x)
+
+    def getMax(self, s: str, a: str, b: str, x: int, y: int):
+        c1 = c2 = ans = 0
+        s += 'c'
+        for c in s:
+            if c == a:
+                c1 += 1
+            elif c == b:
+                if c1 == 0:
+                    c2 += 1
+                else:
+                    ans += x
+                    c1 -= 1
+            else:
+                ans += y * min(c1, c2)
+                c1 = c2 = 0
+        return ans
