84e1f643d1d94ecd3519f64c39e68dee922a813d
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-07-25 10:54:29 +0700

    Create 3487.js

---
+++ JS/easy-solution/3487.js
@@ -0,0 +1,34 @@
+/**
+3487. Maximum Unique Subarray Sum After Deletion
+    You are given an integer array nums.
+    You are allowed to delete any number of elements from nums without making it empty. After performing the deletions, select a subarray of nums such that:
+    All elements in the subarray are unique.
+    The sum of the elements in the subarray is maximized.
+    Return the maximum sum of such a subarray.
+
+    Example :
+    Input: nums = [1,2,3,4,5]
+    Output: 15
+    Explanation:
+    Select the entire array without deleting any element to obtain the maximum sum.
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSum = function(nums) {
+    const maxVal = Math.max(...nums);
+    if (maxVal < 0){
+        return maxVal;
+    }
+    const set = new Set(nums);
+    let res = 0;
+    for (const num of set){
+        if (num >= 0){
+            res += num;
+        }
+    }
+    return res;
+};
