ed81f5d3ca18e2427cb1865b9b79ed480f935ecc
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-07-26 16:02:12 +0700

    Create 3480.py

---
+++ python3/hard-solution/3480.py
@@ -0,0 +1,36 @@
+"""
+3480. Maximize Subarrays After Removing One Conflicting Pair
+    You are given an integer n which represents an array nums containing the numbers from 1 to n in order. Additionally, you are given a 2D array conflictingPairs, where conflictingPairs[i] = [a, b] indicates that a and b form a conflicting pair.
+    Remove exactly one element from conflictingPairs. Afterward, count the number of non-empty subarrays of nums which do not contain both a and b for any remaining conflicting pair [a, b].
+    Return the maximum number of subarrays possible after removing exactly one conflicting pair.
+    Example :
+    Input: n = 4, conflictingPairs = [[2,3],[1,4]]
+    Output: 9
+    Explanation:
+    Remove [2, 3] from conflictingPairs. Now, conflictingPairs = [[1, 4]].
+    There are 9 subarrays in nums where [1, 4] do not appear together. They are [1], [2], [3], [4], [1, 2], [2, 3], [3, 4], [1, 2, 3] and [2, 3, 4].
+    The maximum number of subarrays we can achieve after removing one element from conflictingPairs is 9.
+"""
+
+
+from collections import defaultdict
+from typing import List
+class Solution:
+    def maxSubarrays(self, n: int, conflictingPairs: List[List[int]]) -> int:
+        conflict = defaultdict(list)
+        for u, v in conflictingPairs:
+            conflict[max(u, v)].append(min(u, v))
+        res = 0
+        max_diff, max_left = 0, 0
+        prefix = [0] * (n + 1)
+        for r in range(1, n + 1):
+            for l in conflict[r]:
+                if l > max_diff:
+                    max_diff, max_left = l, max_diff
+                elif l > max_left:
+                    max_left  = l
+            res += r - max_diff
+                
+            if max_diff > 0:
+                prefix[max_diff] += max_diff - max_left
+        return res + max(prefix)
