d6d91a3e5a7f57dcb7cc576bfa451d74c6c16e24
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-08-04 15:39:03 +0700

    Create 904.js

---
+++ JS/medium-solution/904.js
@@ -0,0 +1,46 @@
+/**
+904. Fruit Into Baskets
+    You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+    You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+    You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+    Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+    Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+    Given the integer array fruits, return the maximum number of fruits you can pick.
+
+    Example :
+    Input: fruits = [1,2,1]
+    Output: 3
+    Explanation: We can pick from all 3 trees.
+ */
+
+
+
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function(fruits) {
+    const maxFruits = Math.max(...fruits);
+    const n = fruits.length;
+    const freq = new Array(maxFruits + 1).fill(0);
+    let left = 0, count = 0, baskets = 0, res = 0;
+    for (let right = 0; right < n; right++){
+        const fruit = fruits[right];
+        freq[fruit]++;
+        if (freq[fruit] == 1){
+            baskets++;
+        }
+        count++;
+        while (baskets > 2){
+            const removeFruit = fruits[left];
+            freq[removeFruit]--;
+            if (freq[removeFruit] === 0){
+                baskets--;
+            }
+            left++;
+            count--;
+        }
+        res = Math.max(res, count);
+    }
+    return res;
+};
