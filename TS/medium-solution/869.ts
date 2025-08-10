1195a5de4cbd7bab048566e886d0e46511183b4f
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-08-10 18:17:32 +0700

    Create 869.ts

---
+++ TS/medium-solution/869.ts
@@ -0,0 +1,23 @@
+/**
+869. Reordered Power of 2
+    You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
+    Return true if and only if we can do this so that the resulting number is a power of two.
+
+    Example 1:
+    Input: n = 1
+    Output: true
+ */
+
+
+
+
+function reorderedPowerOf2(n: number): boolean {
+    const s = [...String(n)].sort().join('');
+    for (let i = 0; i < 31; i++){
+        const power = 1 << i;
+        if (s === [...String(power)].sort().join('')){
+            return true;
+        }
+    }
+    return false;
+};
