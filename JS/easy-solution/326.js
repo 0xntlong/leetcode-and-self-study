9bd6909af4968dd92dc07548e495b0b348919539
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-08-13 15:23:37 +0700

    Create 326.js

---
+++ JS/easy-solution/326.js
@@ -0,0 +1,30 @@
+/**
+326. Power of Three
+    Given an integer n, return true if it is a power of three. Otherwise, return false.
+    An integer n is a power of three, if there exists an integer x such that n == 3^x.
+
+    Example :
+
+    Input: n = 27
+    Output: true
+    Explanation: 27 = 3^3
+ */
+
+
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if (n <= 0)
+        return false;
+    while (n > 1){
+        if (n % 3 == 0){
+            n = n / 3;
+        } else {
+            return false;
+        }
+    }
+    return true;
+};
