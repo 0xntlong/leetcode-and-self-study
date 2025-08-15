0fc83d15f1865abc5b3124d34640d7e6bbb529d2
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-08-15 15:31:43 +0700

    Create 342.js

---
+++ JS/easy-solution/342.js
@@ -0,0 +1,32 @@
+/**
+342. Power of Four
+    Given an integer n, return true if it is a power of four. Otherwise, return false.
+    An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+    Example :
+
+    Input: n = 16
+    Output: true
+ */
+
+
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if (n <= 0)
+        return false;
+    let temp = 1;
+    let i = 0;
+    while (true){
+        if (temp === n){
+            return true;
+        } else if (temp > n){
+            return false;
+        }
+        temp *= 4;
+        i++;
+    }
+};
