a53faa019c70c076bd632b6361e5fdd0ed04576f
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-08-16 14:48:03 +0700

    Create 1323.js

---
+++ JS/easy-solution/1323.js
@@ -0,0 +1,32 @@
+/**
+1323. Maximum 69 Number
+    You are given a positive integer num consisting only of digits 6 and 9.
+    Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+    Example :
+    Input: num = 9669
+    Output: 9969
+    Explanation: 
+    Changing the first digit results in 6669.
+    Changing the second digit results in 9969.
+    Changing the third digit results in 9699.
+    Changing the fourth digit results in 9666.
+    The maximum number is 9969.
+ */
+
+
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    let s = num.toString().split('');
+    for (let i = 0; i < s.length; i++){
+        if (s[i] === '6'){
+            s[i] = '9';
+            break;
+        }
+    }
+    return parseInt(s.join(''), 10);
+};
