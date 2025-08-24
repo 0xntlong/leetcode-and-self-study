99407c7dcec224f4fb37177f347d900bb6dda775
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-08-24 16:18:10 +0700

    Create 1493.ts

---
+++ TS/medium-solution/1493.ts
@@ -0,0 +1,28 @@
+/**
+1493. Longest Subarray of 1's After Deleting One Element
+    Given a binary array nums, you should delete one element from it.
+    Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+    Example :
+    Input: nums = [1,1,0,1]
+    Output: 3
+    Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+ */
+
+
+function longestSubarray(nums: number[]): number {
+    const n = nums.length;
+    let l = 0, res = 0, zero = 0;
+    for (let i = 0; i < n; i++){
+        if (nums[i] === 0){
+            zero++;
+        } while (zero > 1){
+            if (nums[l] === 0){
+                zero--;
+            }
+            l++;
+        }
+        res = Math.max(res, i - l);
+    }
+    return res;
+};
