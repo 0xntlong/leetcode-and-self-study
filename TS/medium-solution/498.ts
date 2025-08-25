e3d9856a4eac9f49fb06c0d27e9fda0edd4874d3
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-08-25 16:31:54 +0700

    Create 498.ts

---
+++ TS/medium-solution/498.ts
@@ -0,0 +1,43 @@
+/**
+498. Diagonal Traverse
+    Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+
+    Example 1:
+    Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [1,2,4,7,5,3,6,8,9]
+*/
+
+
+
+function findDiagonalOrder(mat: number[][]): number[] {
+    const m = mat.length, n = mat[0].length;
+    const res: number[] = new Array(m * n);
+    let i = 0, j = 0, dir = 1, idx = 0;
+    for (let cnt = 0; cnt < m * n; cnt++){
+        res[idx++] = mat[i][j];
+        if (dir == 1){
+            if (j === n - 1){
+                i++;
+                dir = -1;
+            } else if (i === 0){
+                j++;
+                dir = -1;
+            } else {
+                i--;
+                j++;
+            }
+        } else {
+            if (i === m - 1){
+                j++;
+                dir = 1;
+            } else if ( j === 0){
+                i++;
+                dir = 1;
+            } else {
+                i++;
+                j--;
+            }
+        }
+    }
+    return res;
+};
