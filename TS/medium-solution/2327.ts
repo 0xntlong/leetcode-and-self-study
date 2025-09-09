d544e91642781fa219761807b517398f234e6d96
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-09-09 16:06:30 +0700

    Create 2327.ts

---
+++ TS/medium-solution/2327.ts
@@ -0,0 +1,42 @@
+/**
+2327. Number of People Aware of a Secret
+    On day 1, one person discovers a secret.
+    You are given an integer delay, which means that each person will share the secret with a new person every day, starting from delay days after discovering the secret. You are also given an integer forget, which means that each person will forget the secret forget days after discovering it. A person cannot share the secret on the same day they forgot it, or on any day afterwards.
+    Given an integer n, return the number of people who know the secret at the end of day n. Since the answer may be very large, return it modulo 109 + 7.
+
+    Example :
+    Input: n = 6, delay = 2, forget = 4
+    Output: 5
+    Explanation:
+    Day 1: Suppose the first person is named A. (1 person)
+    Day 2: A is the only person who knows the secret. (1 person)
+    Day 3: A shares the secret with a new person, B. (2 people)
+    Day 4: A shares the secret with a new person, C. (3 people)
+    Day 5: A forgets the secret, and B shares the secret with a new person, D. (3 people)
+    Day 6: B shares the secret with E, and C shares the secret with F. (5 people)
+ */
+
+
+
+
+function peopleAwareOfSecret(n: number, delay: number, forget: number): number {
+    const mod = 10**9 + 7;
+    let dp: number[] = new Array(n).fill(0);
+    dp[0] = 1;
+    let total = 0;
+    for (let i = delay; i < n; i++) {
+        total += dp[i - delay];
+        dp[i] = total % mod;
+        if (i - forget + 1 >= 0) {
+            total -= dp[i - forget + 1];
+            total = (total + mod) % mod; 
+        }
+    }
+
+    let res = 0;
+    for (let i = n - forget; i < n; i++) {
+        res = (res + dp[i]) % mod;
+    }
+
+    return res;
+};
