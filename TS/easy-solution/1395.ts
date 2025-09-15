3aab5e2e7f1a8501605573c9f94ff7975ed8ffb1
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-09-15 15:43:00 +0700

    Create 1395.ts

---
+++ TS/easy-solution/1395.ts
@@ -0,0 +1,30 @@
+/**
+1935. Maximum Number of Words You Can Type
+    There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+    Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+    Example :
+    Input: text = "hello world", brokenLetters = "ad"
+    Output: 1
+    Explanation: We cannot type "world" because the 'd' key is broken.
+ */
+
+
+
+
+function canBeTypedWords(text: string, brokenLetters: string): number {
+    const st = new Set(brokenLetters.split(""));
+    const words = text.split(" ");
+    let cnt = 0;
+    for (let w of words){
+        let check = true;
+        for (let c of w){
+            if (st.has(c)){
+                check = false;
+                break;
+            }
+        }
+        if (check)
+            cnt++;
+    }
+    return cnt;
+};
