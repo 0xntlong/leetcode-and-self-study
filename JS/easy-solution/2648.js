d49c83836c6fbf398a6ec27137b7fbd3cb115ad2
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-09-17 23:38:45 +0700

    Create 2648.js

---
+++ JS/easy-solution/2648.js
@@ -0,0 +1,37 @@
+/**
+2648. Generate Fibonacci Sequence
+    Write a generator function that returns a generator object which yields the fibonacci sequence.
+    The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+    The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+    Example :
+    Input: callCount = 5
+    Output: [0,1,1,2,3]
+    Explanation:
+    const gen = fibGenerator();
+    gen.next().value; // 0
+    gen.next().value; // 1
+    gen.next().value; // 1
+    gen.next().value; // 2
+    gen.next().value; // 3
+ */
+
+
+
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let prev = 0, cur = 1;
+    while (true){
+        yield prev;
+        next = prev + cur;
+        prev = cur;
+        cur = next;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
