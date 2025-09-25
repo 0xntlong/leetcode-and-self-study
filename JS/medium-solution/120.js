280806579341a146a2b46d1903930ad130022b99
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-09-26 01:36:32 +0700

    Update 120.js

--- JS/medium-solution/120.js
+++ JS/medium-solution/120.js
@@ -7,9 +7,27 @@
     Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
     Output: 11
     Explanation: The triangle looks like:
-    2
-    3 4
-    6 5 7
-    4 1 8 3
+        2
+       3 4
+      6 5 7
+     4 1 8 3
     The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
  */
+
+
+
+
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    let n = triangle.length;
+    let dp = new Array(n + 1).fill(0);
+    for (let row = n - 1; row >= 0; row--){
+        for (let i = 0; i < triangle[row].length; i++){
+            dp[i] = Math.min(dp[i], dp[i + 1]) + triangle[row][i];
+        }
+    }
+    return dp[0];
+};
