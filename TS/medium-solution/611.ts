4bd4eeb51fd4b1990cd3f57557ac83c47883f198
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-09-26 15:52:48 +0700

    Create 611.ts

---
+++ TS/medium-solution/611.ts
@@ -0,0 +1,35 @@
+/**
+611. Valid Triangle Number
+    Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+    Example 1:
+    Input: nums = [2,2,3,4]
+    Output: 3
+    Explanation: Valid combinations are: 
+    2,3,4 (using the first 2)
+    2,3,4 (using the second 2)
+    2,2,3
+ */
+
+
+
+function triangleNumber(nums: number[]): number {
+    nums.sort((a, b) => a - b);
+    let n = nums.length;
+    let count = 0;
+    if (n <= 2){
+        return 0;
+    }
+    for (let k = 2; k < n; k++){
+        let i = 0;
+        let j = k - 1;
+        while (i < j){
+            if (nums[i] + nums[j] > nums[k]){
+                count += j - i;
+                j--;
+            } else {
+                i++;
+            }
+        }
+    }
+    return count;
+};
