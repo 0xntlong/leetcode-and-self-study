d071d6bb1d099ebde86efd49a3fda10ee33cf513
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-10-03 16:28:36 +0700

    Update 407.py

--- python3/hard-solution/407.py
+++ python3/hard-solution/407.py
@@ -11,6 +11,8 @@
 """
 
 from typing import List
+
+
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
         m = len(heightMap)
@@ -43,3 +45,27 @@ class Solution:
                     res += vol[i][j] - heightMap[i][j]
         
         return res
+    
+
+
+
+"""
+import numpy as np
+
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        water_level = np.array(heightMap, dtype = int)
+        heightMap = np.array(heightMap, dtype = int)
+        
+        water_level[1:-1, 1:-1] = np.max(heightMap)
+        water = 0
+        while True:
+            water_pad = np.pad(water_level, ((1, 1), (1, 1)))
+            water_level = np.maximum(np.minimum(np.minimum(water_pad[0:-2, 1: -1], water_pad[2:, 1:-1]),
+                                                np.minimum(water_pad[1:-1, 0:-2], water_pad[1:-1, 2:])), heightMap)
+
+            w = np.sum(water_level - heightMap)
+            if water == w:
+                return int(w)
+            water = w
+"""
