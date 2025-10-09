cbdc28b4dac7687af9427c6af78453796bad6d2c
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-10-09 16:24:36 +0700

    Create 3494.py

---
+++ python3/medium-solution/3494.py
@@ -0,0 +1,44 @@
+"""
+3494. Find the Minimum Amount of Time to Brew Potions
+    You are given two integer arrays, skill and mana, of length n and m, respectively.
+
+    In a laboratory, n wizards must brew m potions in order. Each potion has a mana capacity mana[j] and must pass through all the wizards sequentially to be brewed properly. The time taken by the ith wizard on the jth potion is timeij = skill[i] * mana[j].
+
+    Since the brewing process is delicate, a potion must be passed to the next wizard immediately after the current wizard completes their work. This means the timing must be synchronized so that each wizard begins working on a potion exactly when it arrives. ​
+
+    Return the minimum amount of time required for the potions to be brewed properly.
+
+    Example :
+    Input: skill = [1,5,2,4], mana = [5,1,4,2]
+    Output: 110
+    Explanation:
+    Potion Number	Start time	Wizard 0 done by	Wizard 1 done by	Wizard 2 done by	Wizard 3 done by
+    0	            0	        5	                30	                40	                60
+    1	            52	        53	                58	                60	                64
+    2	            54	        58	                78	                86	                102
+    3	            86	        88	                98	                102	                110
+    As an example for why wizard 0 cannot start working on the 1st potion before time t = 52, consider the case where the wizards started preparing the 1st potion at time t = 50. At time t = 58, wizard 2 is done with the 1st potion, but wizard 3 will still be working on the 0th potion till time t = 60.
+"""
+
+from typing import List
+class Solution:
+    def minTime(self, skill: List[int], mana: List[int]) -> int:
+        n, m = len(skill), len(mana)
+        if n == 0 or m == 0:
+            return 0
+
+        total_skill = sum(skill)
+        
+        time = total_skill * mana[-1]  
+
+        for j in range(m - 1):
+            pre = 0
+            mx = 0
+            for s in skill:
+                need = (pre + s) * mana[j] - pre * mana[j + 1]
+                if need > mx:
+                    mx = need
+                pre += s
+            time += mx
+
+        return time
