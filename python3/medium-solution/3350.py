889f47a29c2c9f3ad94c0fe57d67ad2ccc3767ec
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-10-15 16:06:27 +0700

    Create 3350.py

---
+++ python3/medium-solution/3350.py
@@ -0,0 +1,37 @@
+"""
+3350. Adjacent Increasing Subarrays Detection II
+    Given an array nums of n integers, your task is to find the maximum value of k for which there exist two adjacent subarrays of length k each, such that both subarrays are strictly increasing. Specifically, check if there are two subarrays of length k starting at indices a and b (a < b), where:
+    Both subarrays nums[a..a + k - 1] and nums[b..b + k - 1] are strictly increasing.
+    The subarrays must be adjacent, meaning b = a + k.
+    Return the maximum possible value of k.
+
+    A subarray is a contiguous non-empty sequence of elements within an array.
+    Example :
+    Input: nums = [2,5,7,8,9,2,3,4,3,1]
+    Output: 3
+    Explanation:
+    The subarray starting at index 2 is [7, 8, 9], which is strictly increasing.
+    The subarray starting at index 5 is [2, 3, 4], which is also strictly increasing.
+    These two subarrays are adjacent, and 3 is the maximum possible value of k for which two such adjacent strictly increasing subarrays exist.
+"""
+
+
+from typing import List
+class Solution:
+    def maxIncreasingSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return 0
+        L = [1] * n
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                L[i] = L[i - 1] + 1
+        R = [1] * n
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                R[i] = R[i + 1] + 1
+
+        res = 0
+        for i in range(n -1):
+            res = max(res, min(L[i], R[i + 1]))
+        return res
