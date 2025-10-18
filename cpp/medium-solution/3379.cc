8671e0647ced925668075974835e71ca4dc7f459
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-10-18 16:24:37 +0700

    Create 3379.cc

---
+++ cpp/medium-solution/3379.cc
@@ -0,0 +1,35 @@
+/**
+3397. Maximum Number of Distinct Elements After Operations
+    You are given an integer array nums and an integer k.
+    You are allowed to perform the following operation on each element of the array at most once:
+    Add an integer in the range [-k, k] to the element.
+    Return the maximum possible number of distinct elements in nums after performing the operations.
+
+    Example 1:
+    Input: nums = [1,2,2,3,3,4], k = 2
+    Output: 6
+    Explanation:
+    nums changes to [-1, 0, 1, 2, 3, 4] after performing operations on the first four elements.
+ */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int res = 0;
+        long long prev = LLONG_MIN;
+        for (int i : nums){
+            if (prev < i - k){
+                prev = i - k;
+                res++;
+            } else if (prev < i + k){
+                prev = prev + 1;
+                res++;
+            }
+        }
+        return res;
+    }
+};
