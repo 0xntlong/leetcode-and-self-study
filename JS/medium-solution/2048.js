edeffe6b66952c86f6c6a8486ca65c6b9f747b50
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-10-24 16:09:55 +0700

    Create 2048.js

---
+++ JS/medium-solution/2048.js
@@ -0,0 +1,42 @@
+/**
+2048. Next Greater Numerically Balanced Number
+    An integer x is numerically balanced if for every digit d in the number x, there are exactly d occurrences of that digit in x.
+    Given an integer n, return the smallest numerically balanced number strictly greater than n.
+
+    Example :
+    Input: n = 1
+    Output: 22
+    Explanation: 
+    22 is numerically balanced since:
+    - The digit 2 occurs 2 times. 
+    It is also the smallest numerically balanced number strictly greater than 1.
+ */
+
+
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nextBeautifulNumber = function(n) {
+    function balance(num){
+        const s = num.toString();
+        const freq = new Array(10).fill(0);
+        for (const c of s){
+            freq[+c]++;
+        }
+        for (const c of s){
+            const digit = +c;
+            if (freq[digit] !== digit){
+                return false;
+            }
+        }
+        return true;
+    }
+    n++;
+    while (!balance(n)){
+        n++;
+    }
+    return n;
+};
