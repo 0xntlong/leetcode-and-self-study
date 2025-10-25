a5af9a57828940e12a789d08cc5bafb752d74075
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-10-25 15:45:24 +0700

    Create 1716.js

---
+++ JS/easy-solution/1716.js
@@ -0,0 +1,27 @@
+/*
+1716. Calculate Money in Leetcode Bank
+    Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+    He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+    Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+    Example :
+    Input: n = 4
+    Output: 10
+    Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+*/
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalMoney = function(n) {
+    const weeks = Math.floor(n / 7);
+    const days = n % 7;
+    let total = weeks * 28 + 7 * (weeks * (weeks - 1) / 2);
+    const start = weeks + 1;
+    for (let i = 0; i < days; i++){
+        total += start + i;
+    }
+    return total;
+};
