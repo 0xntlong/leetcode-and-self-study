573696e2f4835ef967a74710d7a4171b88051984
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-10-29 15:19:21 +0700

    Create 3370.py

---
+++ python3/easy-solution/3370.py
@@ -0,0 +1,18 @@
+"""
+3370. Smallest Number With All Set Bits
+    You are given a positive number n.
+    Return the smallest number x greater than or equal to n, such that the binary representation of x contains only set bits
+    Example 1:
+    Input: n = 5
+    Output: 7
+    Explanation:
+    The binary representation of 7 is "111".
+"""
+
+
+
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        while n & (n + 1):
+            n |= n + 1
+        return n
