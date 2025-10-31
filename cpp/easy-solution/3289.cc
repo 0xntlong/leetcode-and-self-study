df83325f5c05684f73a1f80d50fcc24f572629f3
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-10-31 15:58:43 +0700

    Create 3289.cc

---
+++ cpp/easy-solution/3289.cc
@@ -0,0 +1,30 @@
+/**
+3289. The Two Sneaky Numbers of Digitville    
+    In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an additional time, making the list longer than usual.
+    As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
+
+    Example 1:
+    Input: nums = [0,1,1,0]
+    Output: [0,1]
+    Explanation:
+    The numbers 0 and 1 each appear twice in the array.
+ */
+
+#include <vector>
+#include <bitset>
+using namespace std;
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        bitset<100> visited;
+        vector<int> res;
+        for (int i : nums){
+            if (visited[i]){
+                res.push_back(i); 
+            } else {
+                visited[i] = 1;
+            }
+        }
+        return res;
+    }
+};
