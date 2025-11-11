581f810a7cb67d95bcec869c9bd8947342697d0f
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-11-11 23:30:08 +0700

    Create 474.cc

---
+++ cpp/medium-solution/474.cc
@@ -0,0 +1,36 @@
+/**
+474. Ones and Zeroes
+    You are given an array of binary strings strs and two integers m and n.
+    Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
+    A set x is a subset of a set y if all elements of x are also elements of y.
+
+    Example :
+    Input: strs = ["10","0001","111001","1","0"], m = 5, n = 3
+    Output: 4
+    Explanation: The largest subset with at most 5 0's and 3 1's is {"10", "0001", "1", "0"}, so the answer is 4.
+    Other valid but smaller subsets include {"0001", "1"} and {"10", "1", "0"}.
+    {"111001"} is an invalid subset because it contains 4 1's, greater than the maximum of 3.
+ */
+
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (auto& s : strs){
+            int count0 = count(s.begin(), s.end(), '0');
+            int count1 = s.size() - count0;
+            for (int i = m; i >= count0; i--){
+                for (int j = n; j >= count1; j--){
+                    dp[i][j] = max(dp[i][j], dp[i - count0][j - count1] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
