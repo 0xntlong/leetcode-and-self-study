68804d8358f85224129f4daf64c3ad0a2884eb5f
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-11-13 15:53:45 +0700

    Create 3228.ts

---
+++ TS/medium-solution/3228.ts
@@ -0,0 +1,34 @@
+/**
+3228. Maximum Number of Operations to Move Ones to the End 
+    You are given a binary string s.
+    You can perform the following operation on the string any number of times:
+    Choose any index i from the string where i + 1 < s.length such that s[i] == '1' and s[i + 1] == '0'.
+    Move the character s[i] to the right until it reaches the end of the string or another '1'. For example, for s = "010010", if we choose i = 1, the resulting string will be s = "000110".
+    Return the maximum number of operations that you can perform.
+    Example 1:
+    Input: s = "1001101"
+    Output: 4
+    Explanation:
+    We can perform the following operations:
+    Choose index i = 0. The resulting string is s = "0011101".
+    Choose index i = 4. The resulting string is s = "0011011".
+    Choose index i = 3. The resulting string is s = "0010111".
+    Choose index i = 2. The resulting string is s = "0001111".
+ */
+
+
+
+
+function maxOperations(s: string): number {
+    const n : number = s.length;
+    let cnt1 : number = s[0] === '1' ? 1 : 0;
+    let total : number = 0;
+    for (let i = 1; i < n; i++){
+        if (s[i] === '1'){
+            cnt1++;
+        } if (s[i] === '0' && s[i - 1] === '1'){
+            total += cnt1;
+        }
+    }
+    return total;
+};
