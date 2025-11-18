8ea3cdcc536ecd231e5d921399bb7bb8f1c420ea
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-11-18 23:23:24 +0700

    Create 717.py

---
+++ python3/easy-solution/717.py
@@ -0,0 +1,26 @@
+"""
+717. 1-bit and 2-bit Characters
+    We have two special characters:
+    The first character can be represented by one bit 0.
+    The second character can be represented by two bits (10 or 11).
+    Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+
+    Example :
+    Input: bits = [1,0,0]
+    Output: true
+    Explanation: The only way to decode it is two-bit character and one-bit character.
+    So the last character is one-bit character.
+"""
+
+from ast import List
+
+
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+        while i < len(bits) - 1:
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
+        return i == len(bits) - 1
