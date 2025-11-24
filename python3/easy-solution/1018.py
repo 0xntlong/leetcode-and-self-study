e89b703261c1af12da76680962acfef460629e6e
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-11-24 23:07:31 +0700

    Create 1018.py

---
+++ python3/easy-solution/1018.py
@@ -0,0 +1,23 @@
+"""
+1018. Binary Prefix Divisible By 5
+    You are given a binary array nums (0-indexed).
+    We define xi as the number whose binary representation is the subarray nums[0..i] (from most-significant-bit to least-significant-bit).
+    For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
+    Return an array of booleans answer where answer[i] is true if xi is divisible by 5.
+
+    Example :
+    Input: nums = [0,1,1]
+    Output: [true,false,false]
+    Explanation: The input numbers in binary are 0, 01, 011; which are 0, 1, and 3 in base-10.
+    Only the first number is divisible by 5, so answer[0] is true.
+"""
+
+from typing import List
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        c = 0
+        res = []
+        for i in nums:
+            c = (c * 2 + i) % 5
+            res.append(c == 0)
+        return res
