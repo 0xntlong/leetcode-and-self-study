f8064ed4532419a3bca9bde7f3d6306e11551d99
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-11-25 15:15:32 +0700

    Create 1015.cc

---
+++ cpp/medium-solution/1015.cc
@@ -0,0 +1,32 @@
+/**
+1015. Smallest Integer Divisible by K
+    Given a positive integer k, you need to find the length of the smallest positive integer n such that n is divisible by k, and n only contains the digit 1.
+    Return the length of n. If there is no such n, return -1.
+    Note: n may not fit in a 64-bit signed integer.
+
+    Example :
+    Input: k = 1
+    Output: 1
+    Explanation: The smallest answer is n = 1, which has length 1.
+ */
+
+
+
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0){
+            return -1;
+        }
+        long long remain = 0;
+        int l = 0;
+        for (int i = 1; i <= k; i++){
+            remain = (remain * 10 + 1) % k;
+            l++;
+            if (remain == 0){
+                return l;
+            }
+        }
+        return l;
+    }
+};
