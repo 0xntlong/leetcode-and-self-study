85cd13d8b8887f3a7e169b0c5d432128e0b2ec08
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-11-26 16:20:27 +0700

    Create 2435.cc

---
+++ cpp/hard-solution/2435.cc
@@ -0,0 +1,45 @@
+/**
+2435. Paths in Matrix Whose Sum Is Divisible by K
+    You are given a 0-indexed m x n integer matrix grid and an integer k. You are currently at position (0, 0) and you want to reach position (m - 1, n - 1) moving only down or right.
+    Return the number of paths where the sum of the elements on the path is divisible by k. Since the answer may be very large, return it modulo 109 + 7.
+
+    Example :
+    Input: grid = [[5,2,4],[3,0,5],[0,7,2]], k = 3
+    Output: 2
+    Explanation: There are two paths where the sum of the elements on the path is divisible by k.
+    The first path highlighted in red has a sum of 5 + 2 + 4 + 5 + 2 = 18 which is divisible by 3.
+    The second path highlighted in blue has a sum of 5 + 3 + 0 + 5 + 2 = 15 which is divisible by 3.
+ */
+
+
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        const int mod = 1e9 + 7;
+        const int m = grid.size(), n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k, 0)));
+        dp[0][0][grid[0][0] % k] = 1;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                for (int x = 0; x < k; x++){
+                    int new_mod = (x + grid[i][j]) % k;
+                    if (i == 0 && j > 0){
+                        dp[i][j][new_mod] += dp[i][j - 1][x];
+                    }
+                    if (j == 0 && i > 0){
+                        dp[i][j][new_mod] += dp[i - 1][j][x];
+                    }
+                    if (i > 0 && j > 0){
+                        dp[i][j][new_mod] += dp[i - 1][j][x] + dp[i][j - 1][x];
+                    }
+                    dp[i][j][new_mod] %= mod;
+                }
+            }
+        }
+        return dp[m - 1][n - 1][0];
+    }
+};
