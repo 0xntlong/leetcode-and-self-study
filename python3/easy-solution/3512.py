fa31322d9b5a64e516a159fb3a21251d23e2a7e8
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-11-29 16:08:11 +0700

    Create 3512.py

---
+++ python3/easy-solution/3512.py
@@ -0,0 +1,23 @@
+"""
+3512. Minimum Operations to Make Array Sum Divisible by K
+    You are given an integer array nums and an integer k. You can perform the following operation any number of times:
+    Select an index i and replace nums[i] with nums[i] - 1.
+    Return the minimum number of operations required to make the sum of the array divisible by k.
+    Example :
+    Input: nums = [3,9,7], k = 5
+    Output: 4
+    Explanation:
+    Perform 4 operations on nums[1] = 9. Now, nums = [3, 5, 7].
+    The sum is 15, which is divisible by 5.
+"""
+
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        """
+        res = 0
+        for i in range(len(nums)):
+            res += nums[i]
+        return res % k
+        """
+        return sum(nums) % k
