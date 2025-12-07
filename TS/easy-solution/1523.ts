0043ef3fdb4a0697360f98b2833046b9b767c7d1
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-12-07 15:59:20 +0700

    Create 1523.ts

---
+++ TS/easy-solution/1523.ts
@@ -0,0 +1,17 @@
+/*
+1523. Count Odd Numbers in an Interval Range
+    Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+    
+    Example :
+    Input: low = 3, high = 7
+    Output: 3
+    Explanation: The odd numbers between 3 and 7 are [3,5,7].
+*/
+
+
+function countOdds(low: number, high: number): number {
+    if (low & 1) {
+        return Math.floor((high - low + 2) / 2);
+    }
+    return Math.floor((high - low + 1) / 2);
+};
