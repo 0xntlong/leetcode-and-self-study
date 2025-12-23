75bed9cc530f464c9a663dc6a231395fc97b0639
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2025-12-23 14:57:56 +0700

    Update 2054.py

--- python3/medium-solution/2054.py
+++ python3/medium-solution/2054.py
@@ -34,3 +34,25 @@ class Solution:
                 maxSum = max(maxSum, events[i][2] + suffix[nextEvent])
             maxSum = max(maxSum, events[i][2])
         return maxSum            
+    
+
+
+
+"""
+        events.sort(key = lambda x : (x[0], x[1]))
+        n = len(events)
+        start = [e[0] for e in events]
+        suffix = [0] * n
+        suffix[-1] = events[-1][2]
+        for i in range(n - 2, -1, -1):
+            suffix[i] = max(suffix[i + 1], events[i][2])
+
+        res = 0
+        for i in range(n):
+            idx = bisect.bisect_left(start, events[i][1] + 1)
+            val = events[i][2]
+            if idx < n:
+                val += suffix[idx]
+            res = max(res, val)
+        return res
+"""
