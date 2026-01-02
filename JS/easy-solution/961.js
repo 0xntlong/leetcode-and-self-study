2910a350c2a424e152c9e4c118b372b74571d242
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-01-02 11:57:05 +0700

    Create 961.js

---
+++ JS/easy-solution/961.js
@@ -0,0 +1,29 @@
+/**
+961. N-Repeated Element in Size 2N Array
+    You are given an integer array nums with the following properties:
+    nums.length == 2 * n.
+    nums contains n + 1 unique elements.
+    Exactly one element of nums is repeated n times.
+    Return the element that is repeated n times.
+
+    Example :
+    Input: nums = [1,2,3,3]
+    Output: 3
+ */
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+    const set = new Set();
+    for (const i of nums){
+        if (set.has(i)){
+            return i;
+        }
+        set.add(i);
+    }
+};
