328840dff0a794a01ad94b642b85399aed0f5303
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-01-09 12:21:42 +0700

    Create 865.py

---
+++ python3/medium-solution/865.py
@@ -0,0 +1,37 @@
+"""
+865. Smallest Subtree with all the Deepest Nodes
+    Given the root of a binary tree, the depth of each node is the shortest distance to the root.
+    Return the smallest subtree such that it contains all the deepest nodes in the original tree.
+    A node is called the deepest if it has the largest depth possible among any node in the entire tree.
+    The subtree of a node is a tree consisting of that node, plus the set of all descendants of that node.
+
+    Example :
+    Input: root = [3,5,1,6,2,0,8,null,null,7,4]
+    Output: [2,7,4]
+    Explanation: We return the node with value 2, colored in yellow in the diagram.
+    The nodes coloured in blue are the deepest nodes of the tree.
+    Notice that nodes 5, 3 and 2 contain the deepest nodes in the tree but node 2 is the smallest subtree among them, so we return it.
+
+"""
+
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node):
+            if not node:
+                return (0, None)
+            left, left_lca = dfs(node.left)
+            right, right_lca = dfs(node.right)
+            if left > right:
+                return (left + 1, left_lca)
+            elif right > left:
+                return (right + 1, right_lca)
+            else:
+                return (left + 1, node)
+        return dfs(root)[1]
