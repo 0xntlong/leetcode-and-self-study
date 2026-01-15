5286e230848d1ebfdf99da521fb589c851449a1b
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-01-15 12:30:45 +0700

    Update 2943.cc

--- cpp/medium-solution/2943.cc
+++ cpp/medium-solution/2943.cc
@@ -11,3 +11,29 @@
     The left image shows the initial grid formed by the bars. The horizontal bars are [1,2,3,4], and the vertical bars are [1,2,3].
     One way to get the maximum square-shaped hole is by removing horizontal bar 2 and vertical bar 2.
  */
+
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int find(vector<int>& bars){
+        sort(bars.begin(), bars.end());
+        int longest = 2, curr = 2;
+        for (int i = 1; i < bars.size(); i++){
+            if (bars[i] == bars[i - 1] + 1){
+                curr++;
+            } else {
+                curr = 2;
+            }
+            longest = max(curr, longest);
+        }
+        return longest;
+    }
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        int side = min(find(hBars), find(vBars));
+        return side * side;
+    }
+};
