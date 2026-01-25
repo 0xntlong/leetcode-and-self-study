974d76077658f4f16bd3bd261f563d4972eae0c3
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-01-25 14:18:12 +0700

    Update 1984.cc

--- cpp/easy-solution/1984.cc
+++ cpp/easy-solution/1984.cc
@@ -29,3 +29,20 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int minimumDifference(vector<int>& nums, int k) {
+//         if (k == 1)
+//             return 0;
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         int res = INT_MAX;
+//         for (int i = 0; i <= n - k; i++){
+//             int diff = nums[i + k - 1] - nums[i];
+//             res = min(res, diff);
+//         }
+//         return res;
+//     }
+// };
