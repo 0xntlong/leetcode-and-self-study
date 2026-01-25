aacfb06a83f879c294baef132006107338520217
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-01-25 14:18:33 +0700

    Update 1984.py

--- python3/easy-solution/1984.py
+++ python3/easy-solution/1984.py
@@ -25,3 +25,16 @@ class Solution:
             i += 1
             j += 1
         return ans
+    
+
+# class Solution:
+#     def minimumDifference(self, nums: List[int], k: int) -> int:
+#         if k == 1:
+#             return 0
+#         nums.sort()
+#         n = len(nums)
+#         res = float('inf')
+#         for i in range(n - k + 1):
+#             diff = nums[i + k - 1] - nums[i]
+#             res = min(res, diff)
+#         return res
