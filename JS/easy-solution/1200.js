1454b29645272f2af0c44ad4f3144446b6821d16
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-01-26 23:32:54 +0700

    Create 1200.js

---
+++ JS/easy-solution/1200.js
@@ -0,0 +1,33 @@
+/**
+1200. Minimum Absolute Difference
+    Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+    Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+    a, b are from arr
+    a < b
+    b - a equals to the minimum absolute difference of any two elements in arr
+    Example :
+    Input: arr = [4,2,1,3]
+    Output: [[1,2],[2,3],[3,4]]
+    Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+ */
+
+
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+    arr.sort((a, b) => a - b);
+    const n = arr.length;
+    let min_diff = Infinity;
+    for (let i = 0; i < n - 1; i++){
+        min_diff = Math.min(min_diff, arr[i + 1] - arr[i]);
+    }
+    const res = [];
+    for (let i = 0; i < n - 1; i++){
+        if (arr[i + 1] - arr[i] === min_diff){
+            res.push([arr[i], arr[i + 1]]);
+        }
+    }
+    return res;
+};
