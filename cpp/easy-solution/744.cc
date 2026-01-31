c956be37f5e2eaa2f888605f818cd61180a7d5d5
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-01-31 14:40:59 +0700

    Create 744.cc

---
+++ cpp/easy-solution/744.cc
@@ -0,0 +1,40 @@
+/**
+744. Find Smallest Letter Greater Than Target
+    You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+    Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+    Example :
+    Input: letters = ["c","f","j"], target = "a"
+    Output: "c"
+    Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+ */
+
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n = letters.size();
+        int low = 0, high = n - 1;
+        char min_letter = 'z';
+        int count = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (letters[mid] > target) {
+                if (letters[mid] < min_letter) 
+                    min_letter = letters[mid];
+                high = mid - 1;
+                count = 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (count == 0) 
+            return letters[0];
+        return min_letter;
+    }
+};
