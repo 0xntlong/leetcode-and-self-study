f1c6a05a81146d1e3bcd29fcd2e4e2bd23f43fc2
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-02-07 11:45:07 +0700

    Create 1653.js

---
+++ JS/medium-solution/1653.js
@@ -0,0 +1,35 @@
+/**
+1653. Minimum Deletions to Make String Balanced
+    You are given a string s consisting only of characters 'a' and 'b'​​​​.
+    You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+    Return the minimum number of deletions needed to make s balanced.
+
+    Example :
+    Input: s = "aababbab"
+    Output: 2
+    Explanation: You can either:
+    Delete the characters at 0-indexed positions 2 and 6 ("aababbab" -> "aaabbb"), or
+    Delete the characters at 0-indexed positions 3 and 6 ("aababbab" -> "aabbbb").
+ */
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumDeletions = function(s) {
+    let res = 0;
+    let count = 0;
+
+    for (let c of s) {
+        if (c === 'b') {
+            count++;
+        } else {
+            if (count > 0) {
+                res++;
+                count--;
+            }
+        }
+    }
+    return res;
+};
