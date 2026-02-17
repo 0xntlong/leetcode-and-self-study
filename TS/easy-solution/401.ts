1aa0b57a3ef57a22a926d49ecdd40d306deec2aa
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-02-17 17:52:31 +0700

    Create 401.ts

---
+++ TS/easy-solution/401.ts
@@ -0,0 +1,38 @@
+/**
+401. Binary Watch
+    A binary watch has 4 LEDs on the top to represent the hours (0-11), and 6 LEDs on the bottom to represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
+    For example, the below binary watch reads "4:51".
+    Given an integer turnedOn which represents the number of LEDs that are currently on (ignoring the PM), return all possible times the watch could represent. You may return the answer in any order.
+    The hour must not contain a leading zero.
+
+    For example, "01:00" is not valid. It should be "1:00".
+    The minute must consist of two digits and may contain a leading zero.
+    For example, "10:2" is not valid. It should be "10:02".
+
+    Example :
+    Input: turnedOn = 1
+    Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
+ */
+
+
+function readBinaryWatch(turnedOn: number): string[] {
+    const res: string[] = [];
+    const popcount = (x: number): number => {
+        let c = 0;
+        while (x) {
+            c += (x & 1);
+            x >>= 1;
+        }
+        return c;
+    };
+
+    for (let hour = 0; hour < 12; hour++) {
+        for (let minute = 0; minute < 60; minute++) {
+            if (popcount(hour) + popcount(minute) === turnedOn) {
+                const mm = minute < 10 ? `0${minute}` : `${minute}`;
+                res.push(`${hour}:${mm}`);
+            }
+        }
+    }
+    return res;
+};
