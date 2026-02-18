f48a33540f5ed8eb8ca7f92fdc069e534fa7470e
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-02-18 16:16:59 +0700

    Create 693.cc

---
+++ cpp/easy-solution/693.cc
@@ -0,0 +1,25 @@
+/**
+693. Binary Number with Alternating Bits
+    Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+    Example :
+    Input: n = 5
+    Output: true
+    Explanation: The binary representation of 5 is: 101
+ */
+
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int prev = n & 1;
+        n >>= 1;
+        while (n > 0){
+            int curr = n & 1;
+            if (curr == prev)
+                return false;
+            prev = curr;
+            n >>= 1;
+        }
+        return true;
+    }
+};
