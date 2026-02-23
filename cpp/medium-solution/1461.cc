a98afcb1a50cdb0a805c4cdd1854540351372c06
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-02-23 12:45:09 +0700

    Create 1461.cc

---
+++ cpp/medium-solution/1461.cc
@@ -0,0 +1,34 @@
+/**
+1461. Check If a String Contains All Binary Codes of Size K
+    Given a binary string s and an integer k, return true if every binary code of length k is a substring of s. Otherwise, return false.
+    Example :
+    Input: s = "00110110", k = 2
+    Output: true
+    Explanation: The binary codes of length 2 are "00", "01", "10" and "11". They can be all found as substrings at indices 0, 1, 3 and 2 respectively.
+ */
+
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        int req = 1 << k;
+        int n = s.size();
+        if (req > n - k + 1)
+            return false;
+        vector<char> seen(req, 0);
+        int mask = req - 1;
+        int code = 0;
+        int found = 0;
+        for (int i = 0; i < n; i++){
+            code = ((code << 1) & mask) | (s[i] == '1');
+            if (i >= k - 1 && !seen[code]){
+                seen[code] = 1;
+                if (++found == req)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
