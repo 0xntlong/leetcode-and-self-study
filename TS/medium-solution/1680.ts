3e5da2216a018964d3c7dc0067bfd150b4c4f278
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-02-28 12:15:22 +0700

    Create 1680.ts

---
+++ TS/medium-solution/1680.ts
@@ -0,0 +1,24 @@
+/**
+1680. Concatenation of Consecutive Binary Numbers
+    Given an integer n, return the decimal value of the binary string formed by concatenating the binary representations of 1 to n in order, modulo 109 + 7.
+
+    Example :
+    Input: n = 1
+    Output: 1
+    Explanation: "1" in binary corresponds to the decimal value 1. 
+ */
+
+
+
+
+function concatenatedBinary(n: number): number {
+    const MOD = 1000000007n;
+    let res = 0n;
+
+    for (let i = 1n; i <= BigInt(n); i++) {
+        const bits = i.toString(2).length;
+        res = ((res << BigInt(bits)) + i) % MOD;
+    }
+
+    return Number(res);
+};
