42324687deb656202aaa539326dd131b3e2917fd
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-03-17 14:49:08 +0700

    Create 1727.ts

---
+++ TS/medium-solution/1727.ts
@@ -0,0 +1,37 @@
+/**
+1727. Largest Submatrix With Rearrangements
+    You are given a binary matrix matrix of size m x n, and you are allowed to rearrange the columns of the matrix in any order.
+    Return the area of the largest submatrix within matrix where every element of the submatrix is 1 after reordering the columns optimally.
+
+    Example :
+    Input: matrix = [[0,0,1],[1,1,1],[1,0,1]]
+    Output: 4
+    Explanation: You can rearrange the columns as shown above.
+    The largest submatrix of 1s, in bold, has an area of 4.
+ */
+
+
+
+function largestSubmatrix(matrix: number[][]): number {
+    if (matrix.length === 0) 
+        return 0;
+
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    const heights: number[] = new Array(n).fill(0);
+    let maxArea = 0;
+
+    for (const row of matrix) {
+
+        for (let j = 0; j < n; j++)
+            heights[j] = row[j] ? heights[j] + 1 : 0;
+
+        const sortedH = [...heights].sort((a, b) => b - a);
+
+        for (let j = 0; j < n; j++)
+            maxArea = Math.max(maxArea, sortedH[j] * (j + 1));
+    }
+
+    return maxArea;
+};
