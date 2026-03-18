6a8f76fc7f3adc689758cd6ef873f0610b7467d5
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-03-18 15:04:49 +0700

    Create 3070.ts

---
+++ TS/medium-solution/3070.ts
@@ -0,0 +1,31 @@
+/**
+3070. Count Submatrices with Top-Left Element and Sum Less Than k
+    You are given a 0-indexed integer matrix grid and an integer k.re4ắ32
+    Return the number of submatrices that contain the top-left element of the grid, and have a sum less than or equal to k.
+    Example :
+    Input: grid = [[7,6,3],[6,6,1]], k = 18
+    Output: 4
+    Explanation: There are only 4 submatrices, shown in the image above, that contain the top-left element of grid, and have a sum less than or equal to 18.
+
+ */
+
+
+function countSubmatrices(grid: number[][], k: number): number {
+    const r = grid.length;
+    const c = grid[0].length;
+
+    const cols: number[] = new Array(c).fill(0);
+    let res = 0;
+
+    for (let i = 0; i < r; i++) {
+        let rowSum = 0;
+        for (let j = 0; j < c; j++) {
+            cols[j] += grid[i][j];
+            rowSum += cols[j];
+            if (rowSum <= k)
+                res++;
+        }
+    }
+
+    return res;
+};
