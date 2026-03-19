3c72e3ea99e5929a4ebdbb8e8d4244372f28b5bb
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-03-19 11:16:10 +0700

    Create 3212.ts

---
+++ TS/medium-solution/3212.ts
@@ -0,0 +1,47 @@
+/**
+3212. Count Submatrices With Equal Frequency of X and Y
+    Given a 2D character matrix grid, where grid[i][j] is either 'X', 'Y', or '.', return the number of submatrices that contain:
+    grid[0][0]
+    an equal frequency of 'X' and 'Y'.
+    at least one 'X'.
+
+    Example :
+
+    Input: grid = [["X","Y","."],["Y",".","."]]
+
+    Output: 3
+ */
+
+
+
+function numberOfSubmatrices(grid: string[][]): number {
+    const m = grid.length;
+    const n = grid[0].length;
+
+    const ox: number[] = new Array(n).fill(0);
+    const oy: number[] = new Array(n).fill(0);
+
+    let res = 0;
+
+    for (let i = 0; i < m; i++) {
+        let rowX = 0;
+        let rowY = 0;
+
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 'X') {
+                rowX++;
+            } else if (grid[i][j] === 'Y') {
+                rowY++;
+            }
+
+            ox[j] += rowX;
+            oy[j] += rowY;
+
+            if (ox[j] === oy[j] && ox[j] > 0) {
+                res++;
+            }
+        }
+    }
+
+    return res;
+};
