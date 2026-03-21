120555fe921b35023478ccaf7ce1a490614d61af
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-03-21 15:14:58 +0700

    Create 3643.ts

---
+++ TS/easy-solution/3643.ts
@@ -0,0 +1,32 @@
+/**
+3643. Flip Square Submatrix Vertically
+    You are given an m x n integer matrix grid, and three integers x, y, and k.
+    The integers x and y represent the row and column indices of the top-left corner of a square submatrix and the integer k represents the size (side length) of the square submatrix.
+    Your task is to flip the submatrix by reversing the order of its rows vertically.
+    Return the updated matrix.
+
+    Example :
+    Input: grid = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], x = 1, y = 0, k = 3
+    Output: [[1,2,3,4],[13,14,15,8],[9,10,11,12],[5,6,7,16]]
+    Explanation:
+    The diagram above shows the grid before and after the transformation.
+ */
+
+
+
+function reverseSubmatrix(grid: number[][], x: number, y: number, k: number): number[][] {
+    let l = x;
+    let r = x + k - 1;
+
+    while (l <= r) {
+        for (let i = y; i < y + k; i++) {
+            const temp = grid[l][i];
+            grid[l][i] = grid[r][i];
+            grid[r][i] = temp;
+        }
+        l++;
+        r--;
+    }
+
+    return grid;
+};
