9dad9c72225d63c3988e1c77621b6fbbfa44e900
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-04-08 15:33:01 +0700

    Create 3653.js

---
+++ JS/medium-solution/3653.js
@@ -0,0 +1,44 @@
+/**
+3653. XOR After Range Multiplication Queries I
+    You are given an integer array nums of length n and a 2D integer array queries of size q, where queries[i] = [li, ri, ki, vi].
+
+    For each query, you must apply the following operations in order:
+    Set idx = li.
+    While idx <= ri:
+    Update: nums[idx] = (nums[idx] * vi) % (109 + 7)
+    Set idx += ki.
+    Return the bitwise XOR of all elements in nums after processing all queries.
+    Example :
+    Input: nums = [1,1,1], queries = [[0,2,1,4]]
+
+    Output: 4
+
+    Explanation:
+
+    A single query [0, 2, 1, 4] multiplies every element from index 0 through index 2 by 4.
+    The array changes from [1, 1, 1] to [4, 4, 4].
+    The XOR of all elements is 4 ^ 4 ^ 4 = 4.
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @param {number[][]} queries
+ * @return {number}
+ */
+var xorAfterQueries = function(nums, queries) {
+    const mod = 1e9 + 7;
+
+    for (let [l, r, k, v] of queries) {
+        for (let i = l; i <= r; i += k) {
+            nums[i] = (nums[i] * v) % mod;
+        }
+    }
+
+    let res = 0;
+    for (let x of nums) {
+        res ^= x;
+    }
+
+    return res;
+};
