c3887f843f134cca433bbe01132ded3aff03b07d
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-04-15 11:48:23 +0700

    Create 2515.js

---
+++ JS/easy-solution/2515.js
@@ -0,0 +1,38 @@
+/*
+2515. Shortest Distance to a Target String in Circular Array
+    You are given a 0-indexed circular string array words and a string target. A circular array means that the array's end connects to the array's beginning.
+    Formally, the next element of words[i] is words[(i + 1) % n] and the previous element of words[i] is words[(i - 1 + n) % n], where n is the length of words.
+    Starting from startIndex, you can move to either the next word or the previous word with 1 step at a time.
+    Return the shortest distance needed to reach the string target. If the string target does not exist in words, return -1.
+
+    Example :
+    Input: words = ["hello","i","am","leetcode","hello"], target = "hello", startIndex = 1
+    Output: 1
+    Explanation: We start from index 1 and can reach "hello" by
+    - moving 3 units to the right to reach index 4.
+    - moving 2 units to the left to reach index 4.
+    - moving 4 units to the right to reach index 0.
+    - moving 1 unit to the left to reach index 0.
+    The shortest distance to reach "hello" is 1.
+*/
+
+
+/**
+ * @param {string[]} words
+ * @param {string} target
+ * @param {number} startIndex
+ * @return {number}
+ */
+var closestTarget = function(words, target, startIndex) {
+    const n = words.length;
+    let res = n;
+
+    for (let i = 0; i < n; i++) {
+        if (words[i] === target) {
+            const distance = Math.abs(i - startIndex);
+            res = Math.min(res, Math.min(n - distance, distance));
+        }
+    }
+
+    return res === n ? -1 : res;
+};
