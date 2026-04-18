159552c63df1f44e381fb2c0fa0e53c8ef9eb15c
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-04-18 09:55:25 +0700

    Create 3761.js

---
+++ JS/medium-solution/3761.js
@@ -0,0 +1,59 @@
+/**
+ * 3761. Minimum Absolute Distance Between Mirror Pairs
+    You are given an integer array nums.
+
+    A mirror pair is a pair of indices (i, j) such that:
+
+    0 <= i < j < nums.length, and
+    reverse(nums[i]) == nums[j], where reverse(x) denotes the integer formed by reversing the digits of x. Leading zeros are omitted after reversing, for example reverse(120) = 21.
+    Return the minimum absolute distance between the indices of any mirror pair. The absolute distance between indices i and j is abs(i - j).
+
+    If no mirror pair exists, return -1.
+
+    
+
+    Example 1:
+
+    Input: nums = [12,21,45,33,54]
+
+    Output: 1
+
+    Explanation:
+
+    The mirror pairs are:
+
+    (0, 1) since reverse(nums[0]) = reverse(12) = 21 = nums[1], giving an absolute distance abs(0 - 1) = 1.
+    (2, 4) since reverse(nums[2]) = reverse(45) = 54 = nums[4], giving an absolute distance abs(2 - 4) = 2.
+    The minimum absolute distance among all pairs is 1.
+ */
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMirrorPairDistance = function(nums) {
+    const map = new Map();
+    let mini = Infinity;
+
+    const reverseNum = (num) => {
+        let rev = 0;
+        while (num > 0) {
+            rev = rev * 10 + num % 10;
+            num = Math.floor(num / 10);
+        }
+        return rev;
+    };
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (map.has(num)) {
+            mini = Math.min(mini, i - map.get(num));
+        }
+        const rev = reverseNum(num);
+        map.set(rev, i);
+    }
+
+    return mini === Infinity ? -1 : mini;
+};
