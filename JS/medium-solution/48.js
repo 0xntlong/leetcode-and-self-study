6b842d3a99711a50be5de6e03aed46777cc8db03
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-05-04 15:02:14 +0700

    Create 48.js

---
+++ JS/medium-solution/48.js
@@ -0,0 +1,31 @@
+/**
+48. Rotate Image
+    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+    You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+    Example :
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [[7,4,1],[8,5,2],[9,6,3]]
+ */
+
+
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    const n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= i; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < Math.floor(n / 2); j++) {
+            [matrix[i][j], matrix[i][n - 1 - j]] =
+            [matrix[i][n - 1 - j], matrix[i][j]];
+
+        }
+
+    }
+};
