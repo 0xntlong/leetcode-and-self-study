8d3bdb500e3559d7fbfb20d66459905977c9e59a
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-05-19 14:55:53 +0700

    Update 2540.cc

--- cpp/easy-solution/2540.cc
+++ cpp/easy-solution/2540.cc
@@ -14,16 +14,21 @@ using namespace std;
 class Solution {
     public:
         int getCommon(vector<int>& nums1, vector<int>& nums2) {
-            int i = 0, j = 0;
-            int n = nums1.size(), m = nums2.size();
-            while (i < n && j < m){
-                if (nums1[i] == nums2[j]){
-                    return nums1[i];
-                } else if (nums1[i] < nums2[j]){
-                    i++;
-                } else {
-                    j++;
-                }
+            int s1 = 0;
+            int s2 = 0;
+            int n1 = nums1.size();
+            int n2 = nums2.size();
+            if (nums1[n1 - 1] < nums2[0])
+                return -1;
+            if (nums2[n2 - 1] < nums1[0])
+                return -1;
+            while (s1 < n1 && s2 < n2) {
+                if (nums1[s1] == nums2[s2])
+                    return nums1[s1];
+                else if (nums1[s1] < nums2[s2])
+                    s1++;
+                else
+                    s2++;
             }
             return -1;
         }
