d5dd315b02964b33f9bbd368afe6a74d7c657a43
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-05-24 15:25:41 +0700

    Create 1340.cc

---
+++ cpp/hard-solution/1340.cc
@@ -0,0 +1,50 @@
+/**
+1340. Jump Game V
+    Given an array of integers arr and an integer d. In one step you can jump from index i to index:
+    i + x where: i + x < arr.length and  0 < x <= d.
+    i - x where: i - x >= 0 and  0 < x <= d.
+    In addition, you can only jump from index i to index j if arr[i] > arr[j] and arr[i] > arr[k] for all indices k between i and j (More formally min(i, j) < k < max(i, j)).
+    You can choose any index of the array and start jumping. Return the maximum number of indices you can visit.
+    Notice that you can not jump outside of the array at any time.
+
+    Example :
+    Input: arr = [6,4,14,6,8,13,9,7,10,6,12], d = 2
+    Output: 4
+    Explanation: You can start at index 10. You can jump 10 --> 8 --> 6 --> 7 as shown.
+    Note that if you start at index 6 you can only jump to index 7. You cannot jump to index 5 because 13 > 9. You cannot jump to index 4 because index 5 is between index 4 and 6 and 13 > 9.
+    Similarly You cannot jump from index 3 to index 2 or index 1.
+ */
+
+
+
+class Solution {
+public:
+    int maxJumps(vector<int>& arr, int d) {
+        int n = arr.size();
+        vector<int> steps(n, 1);
+        vector<int> stack;
+        for (int i = 0; i <= n; i++) {
+            while (!stack.empty() && (i == n || arr[stack.back()] < arr[i])) {
+                vector<int> pop_indices;
+                pop_indices.push_back(stack.back());
+                stack.pop_back();
+                while (!stack.empty() && arr[stack.back()] == arr[pop_indices[0]]) {
+                    pop_indices.push_back(stack.back());
+                    stack.pop_back();
+                }
+                for (int j : pop_indices) {
+                    if (i < n && i - j <= d) {
+                        steps[i] = max(steps[i], steps[j] + 1);
+                    }
+                    if (!stack.empty() && j - stack.back() <= d) {
+                        steps[stack.back()] = max(steps[stack.back()], steps[j] + 1);
+                    }
+                }
+            }
+            if (i < n) {
+                stack.push_back(i);
+            }
+        }
+        return *max_element(steps.begin(), steps.end());
+    }
+};
