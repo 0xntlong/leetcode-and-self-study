7038d67ac106492c969a7538904b6527ab73cb28
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-06-05 16:11:08 +0700

    Create 3753.js

---
+++ JS/hard-solution/3753.js
@@ -0,0 +1,92 @@
+/**
+3753. Total Waviness of Numbers in Range II
+    You are given two integers num1 and num2 representing an inclusive range [num1, num2].
+    The waviness of a number is defined as the total count of its peaks and valleys:
+    A digit is a peak if it is strictly greater than both of its immediate neighbors.
+    A digit is a valley if it is strictly less than both of its immediate neighbors.
+    The first and last digits of a number cannot be peaks or valleys.
+    Any number with fewer than 3 digits has a waviness of 0.
+    Return the total sum of waviness for all numbers in the range [num1, num2].
+
+    Example :
+    Input: num1 = 120, num2 = 130
+    Output: 3
+    Explanation:
+    In the range [120, 130]:
+
+    120: middle digit 2 is a peak, waviness = 1.
+    121: middle digit 2 is a peak, waviness = 1.
+    130: middle digit 3 is a peak, waviness = 1.
+    All other numbers in the range have a waviness of 0.
+    Thus, total waviness is 1 + 1 + 1 = 3.
+ */
+
+
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var totalWaviness = function(num1, num2) {
+
+    const getDir = (a, b) => {
+        if (a < b) 
+            return 0;
+        if (a === b) 
+            return 1;
+        return 2;
+    };
+    const matchDir = (dir1, dir2) => {
+        return (dir1 === 0 && dir2 === 2) || (dir1 === 2 && dir2 === 0);
+    };
+    const getIndex = (pos, tie, lastDir, lastDigit) => {
+        return ((pos * 2 + tie) * 3 + lastDir) * 10 + lastDigit;
+    };
+    const helper = (num) => {
+        if (num <= 0) 
+            return 0;
+        const digits = String(num).split('').map(Number);
+        const m = digits.length;
+        const size = m * 2 * 3 * 10;
+        const totalWaviness = new Array(size).fill(0);
+        const totalWays = new Array(size).fill(0);
+        for (let firstPos = 0; firstPos < m; firstPos++) {
+            const maxFirstDigit = firstPos === 0 ? digits[0] : 9;
+            for (let firstDigit = 1; firstDigit <= maxFirstDigit; firstDigit++) {
+                const tie = firstPos === 0 ? (firstDigit < digits[0] ? 1 : 0) : 1;
+                const lastDir = 1;
+                totalWays[getIndex(firstPos, tie, lastDir, firstDigit)]++;
+            }
+        }
+        for (let pos = 0; pos + 1 < m; pos++) {
+            for (let tie = 0; tie < 2; tie++) {
+                for (let lastDir = 0; lastDir < 3; lastDir++) {
+                    for (let lastDigit = 0; lastDigit < 10; lastDigit++) {
+                        const idx = getIndex(pos, tie, lastDir, lastDigit);
+                        if (totalWays[idx] === 0)
+                            continue;
+                        const maxNextDigit = tie ? 9 : digits[pos + 1];
+                        for (let nextDigit = 0; nextDigit <= maxNextDigit; nextDigit++) {
+                            const newTie = tie || nextDigit < digits[pos + 1] ? 1 : 0;
+                            const newDir = getDir(lastDigit, nextDigit);
+                            const newIdx = getIndex(pos + 1, newTie, newDir, nextDigit);
+                            totalWaviness[newIdx] += totalWaviness[idx] + totalWays[idx] * (matchDir(lastDir, newDir) ? 1 : 0);
+                            totalWays[newIdx] += totalWays[idx];
+                        }
+                    }
+                }
+            }
+        }
+        let ans = 0;
+        for (let tie = 0; tie < 2; tie++) {
+            for (let lastDir = 0; lastDir < 3; lastDir++) {
+                for (let lastDigit = 0; lastDigit < 10; lastDigit++) {
+                    ans += totalWaviness[getIndex(m - 1, tie, lastDir, lastDigit)];
+                }
+            }
+        }
+        return ans;
+    };
+    return helper(num2) - helper(num1 - 1);
+};
