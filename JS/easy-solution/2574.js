ce7274e2847518befd102f7c6e79110cf2252b22
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-06-06 15:39:29 +0700

    Create 2574.js

---
+++ JS/easy-solution/2574.js
@@ -0,0 +1,31 @@
+/**
+2574. Left and Right Sum Differences
+    You are given a 0-indexed integer array nums of size n.
+    Define two arrays leftSum and rightSum where:
+    leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+    rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+    Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
+    Example :
+    Input: nums = [10,4,8,3]
+    Output: [15,1,11,22]
+    Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+    The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+ */
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var leftRightDifference = function(nums) {
+    let prefix = 0;
+    let suffix = nums.reduce((a, b) => a + b, 0);
+    const res = [];
+    for (const i of nums) {
+        prefix += i;
+        res.push(Math.abs(prefix - suffix));
+        suffix -= i;
+    }
+    return res;
+};
