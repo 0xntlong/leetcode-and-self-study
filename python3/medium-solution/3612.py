b41473be61da7fd62762ee3525ac3c01129c1c30
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-06-16 15:02:14 +0700

    Create 3612.py

---
+++ python3/medium-solution/3612.py
@@ -0,0 +1,45 @@
+"""
+3612. Process String with Special Operations I
+    You are given a string s consisting of lowercase English letters and the special characters: *, #, and %.
+
+    Build a new string result by processing s according to the following rules from left to right:
+
+    If the letter is a lowercase English letter append it to result.
+    A '*' removes the last character from result, if it exists.
+    A '#' duplicates the current result and appends it to itself.
+    A '%' reverses the current result.
+    Return the final string result after processing all characters in s.
+
+    
+
+    Example 1:
+
+    Input: s = "a#b%*"
+
+    Output: "ba"
+
+    Explanation:
+
+    i	s[i]	Operation	Current result
+    0	'a'	Append 'a'	"a"
+    1	'#'	Duplicate result	"aa"
+    2	'b'	Append 'b'	"aab"
+    3	'%'	Reverse result	"baa"
+    4	'*'	Remove the last character	"ba"
+    Thus, the final result is "ba".
+"""
+
+
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = ""
+        for c in s:
+            if c == "*":
+                res = res[:-1]
+            elif c == "#":
+                res += res
+            elif c == "%":
+                res = res[::-1]
+            else:
+                res += c
+        return res
