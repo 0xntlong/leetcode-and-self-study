d36e23fb30b3dabd25a4b220fae2579d2636cbe3
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-06-18 15:15:07 +0700

    Create 1344.py

---
+++ python3/medium-solution/1344.py
@@ -0,0 +1,14 @@
+"""
+1344. Angle Between Hands of a Clock
+    Given two numbers, hour and minutes, return the smaller angle (in degrees) formed between the hour and the minute hand.
+    Answers within 10-5 of the actual value will be accepted as correct.
+
+    Example :
+    Input: hour = 12, minutes = 30
+    Output: 165
+"""
+
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        diff = abs(hour * 30.0 - minutes * 5.5)
+        return min(diff, 360 - diff)
