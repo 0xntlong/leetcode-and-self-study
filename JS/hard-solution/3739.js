50db1ed9f0bb9386eeac22492ad395ef2b5d5b5c
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-06-26 10:58:03 +0700

    Create 3739.js

---
+++ JS/hard-solution/3739.js
@@ -0,0 +1,51 @@
+/**
+3739. Count Subarrays With Majority Element II
+    You are given an integer array nums and an integer target.
+    Return the number of subarrays of nums in which target is the majority element.
+    The majority element of a subarray is the element that appears strictly more than half of the times in that subarray.
+
+    Example :
+    Input: nums = [1,2,2,3], target = 2
+    Output: 5
+    Explanation:
+    Valid subarrays with target = 2 as the majority element:
+    nums[1..1] = [2]
+    nums[2..2] = [2]
+    nums[1..2] = [2,2]
+    nums[0..2] = [1,2,2]
+    nums[1..3] = [2,2,3]
+    So there are 5 such subarrays.
+ */
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countMajoritySubarrays = function(nums, target) {
+    const n = nums.length;
+    const freq = Array(2 * n + 1).fill(0);
+
+    freq[n] = 1;
+
+    let idx = n;
+    let res = 0;
+    let pref = 0;
+
+    for (const x of nums) {
+        if (x === target) {
+            pref += freq[idx];
+            idx++;
+        } else {
+            idx--;
+            pref -= freq[idx];
+        }
+
+        freq[idx]++;
+        res += pref;
+    }
+
+    return res;
+};
