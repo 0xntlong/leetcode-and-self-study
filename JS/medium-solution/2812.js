6929dbe4f117ccbb6e30ef77dc8b4f6f44541214
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-01 16:12:02 +0700

    Create 2812.js

---
+++ JS/medium-solution/2812.js
@@ -0,0 +1,140 @@
+/**
+2812. Find the Safest Path in a Grid
+    You are given a 0-indexed 2D matrix grid of size n x n, where (r, c) represents:
+    A cell containing a thief if grid[r][c] = 1
+    An empty cell if grid[r][c] = 0
+    You are initially positioned at cell (0, 0). In one move, you can move to any adjacent cell in the grid, including cells containing thieves.
+    The safeness factor of a path on the grid is defined as the minimum manhattan distance from any cell in the path to any thief in the grid.
+    Return the maximum safeness factor of all paths leading to cell (n - 1, n - 1).
+    An adjacent cell of cell (r, c), is one of the cells (r, c + 1), (r, c - 1), (r + 1, c) and (r - 1, c) if it exists.
+    The Manhattan distance between two cells (a, b) and (x, y) is equal to |a - x| + |b - y|, where |val| denotes the absolute value of val.
+    Example :
+    Input: grid = [[1,0,0],[0,0,0],[0,0,1]]
+    Output: 0
+    Explanation: All paths from (0, 0) to (n - 1, n - 1) go through the thieves in cells (0, 0) and (n - 1, n - 1).
+ */
+
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maximumSafenessFactor = function(grid) {
+    class MaxHeap {
+        constructor() {
+            this.heap = [];
+        }
+
+        push(node) {
+            this.heap.push(node);
+            let i = this.heap.length - 1;
+
+            while (i > 0) {
+                let p = Math.floor((i - 1) / 2);
+
+                if (this.heap[p][0] >= this.heap[i][0])
+                    break;
+
+                [this.heap[p], this.heap[i]] = [this.heap[i], this.heap[p]];
+                i = p;
+            }
+        }
+
+        pop() {
+            if (this.heap.length === 1)
+                return this.heap.pop();
+
+            const top = this.heap[0];
+            this.heap[0] = this.heap.pop();
+
+            let i = 0;
+
+            while (true) {
+                let l = i * 2 + 1;
+                let r = i * 2 + 2;
+                let best = i;
+
+                if (l < this.heap.length && this.heap[l][0] > this.heap[best][0])
+                    best = l;
+
+                if (r < this.heap.length && this.heap[r][0] > this.heap[best][0])
+                    best = r;
+
+                if (best === i)
+                    break;
+
+                [this.heap[i], this.heap[best]] = [this.heap[best], this.heap[i]];
+                i = best;
+            }
+
+            return top;
+        }
+
+        size() {
+            return this.heap.length;
+        }
+    }
+
+    const n = grid.length;
+
+    if (grid[0][0] === 1 || grid[n - 1][n - 1] === 1)
+        return 0;
+
+    const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    const dist = Array.from({ length: n }, () => Array(n).fill(Infinity));
+
+    const q = [];
+    let head = 0;
+
+    for (let r = 0; r < n; r++) {
+        for (let c = 0; c < n; c++) {
+            if (grid[r][c] === 1) {
+                dist[r][c] = 0;
+                q.push([r, c]);
+            }
+        }
+    }
+
+    while (head < q.length) {
+        const [r, c] = q[head++];
+
+        for (const [dr, dc] of dirs) {
+            const nr = r + dr;
+            const nc = c + dc;
+
+            if (nr >= 0 && nr < n && nc >= 0 && nc < n && dist[nr][nc] === Infinity) {
+                dist[nr][nc] = dist[r][c] + 1;
+                q.push([nr, nc]);
+            }
+        }
+    }
+
+    const seen = Array.from({ length: n }, () => Array(n).fill(false));
+    const heap = new MaxHeap();
+
+    heap.push([dist[0][0], 0, 0]);
+
+    while (heap.size() > 0) {
+        const [safe, r, c] = heap.pop();
+
+        if (seen[r][c])
+            continue;
+
+        seen[r][c] = true;
+
+        if (r === n - 1 && c === n - 1)
+            return safe;
+
+        for (const [dr, dc] of dirs) {
+            const nr = r + dr;
+            const nc = c + dc;
+
+            if (nr >= 0 && nr < n && nc >= 0 && nc < n && !seen[nr][nc]) {
+                const newSafe = Math.min(safe, dist[nr][nc]);
+                heap.push([newSafe, nr, nc]);
+            }
+        }
+    }
+
+    return -1;
+};
