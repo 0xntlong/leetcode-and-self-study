7b4f7518ab12afa240339aec60e3c5bfe9859034
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-06 12:13:03 +0700

    Create 1288.js

---
+++ JS/medium-solution/1288.js
@@ -0,0 +1,30 @@
+/**
+1288. Remove Covered Intervals
+    Given an array intervals where intervals[i] = [li, ri] represent the interval [li, ri), remove all intervals that are covered by another interval in the list.
+    The interval [a, b) is covered by the interval [c, d) if and only if c <= a and b <= d.
+    Return the number of remaining intervals.
+    Example :
+    Input: intervals = [[1,4],[3,6],[2,8]]
+    Output: 2
+    Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+ */
+
+
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var removeCoveredIntervals = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    let [left, right] = intervals[0];
+    let res = 1;
+    for (const [x, y] of intervals) {
+        if (x > left && y > right) {
+            left = x;
+            res++;
+        }
+        right = Math.max(right, y);
+    }
+    return res;
+};
