74bded1c791cbd7f184795bac27543fa87a6df29
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-07 14:17:35 +0700

    Create 3754.ts

---
+++ TS/easy-solution/3754.ts
@@ -0,0 +1,48 @@
+/**
+3754. Concatenate Non-Zero Digits and Multiply by Sum I
+    You are given an integer n.
+    Form a new integer x by concatenating all the non-zero digits of n in their original order. If there are no non-zero digits, x = 0.
+    Let sum be the sum of digits in x.
+    Return an integer representing the value of x * sum.
+
+    Example:
+    Input: n = 10203004
+    Output: 12340
+    Explanation:
+    The non-zero digits are 1, 2, 3, and 4. Thus, x = 1234.
+    The sum of digits is sum = 1 + 2 + 3 + 4 = 10.
+    Therefore, the answer is x * sum = 1234 * 10 = 12340.
+ */
+
+
+
+function sumAndMultiply(n: number): number {
+    if (n === 0) {
+        return 0;
+    }
+
+    const arr: number[] = [];
+
+    while (n > 0) {
+        const d = n % 10;
+        if (d > 0) {
+            arr.push(d);
+        }
+        n = Math.floor(n / 10);
+    }
+
+    let sum = 0;
+    for (const d of arr) {
+        sum += d;
+    }
+
+    let value = 0;
+    let tens = 1;
+
+    for (const d of arr) {
+        value += d * tens;
+        tens *= 10;
+    }
+
+    return value * sum;
+}
