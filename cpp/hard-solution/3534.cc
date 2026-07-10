795eb432daccfa2e93fa7fc60ac2bb9b94536aed
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-10 14:42:24 +0700

    Create 3534.cc

---
+++ cpp/hard-solution/3534.cc
@@ -0,0 +1,94 @@
+/**
+3534. Path Existence Queries in a Graph II
+    You are given an integer n representing the number of nodes in a graph, labeled from 0 to n - 1.
+    You are also given an integer array nums of length n and an integer maxDiff.
+    An undirected edge exists between nodes i and j if the absolute difference between nums[i] and nums[j] is at most maxDiff (i.e., |nums[i] - nums[j]| <= maxDiff).
+    You are also given a 2D integer array queries. For each queries[i] = [ui, vi], find the minimum distance between nodes ui and vi. If no path exists between the two nodes, return -1 for that query.
+    Return an array answer, where answer[i] is the result of the ith query.
+    Note: The edges between the nodes are unweighted.
+
+    Example:
+    Input: n = 5, nums = [1,8,3,4,2], maxDiff = 3, queries = [[0,3],[2,4]]
+    Output: [1,1]
+    Explanation:
+    The resulting graph is:
+
+    Query	Shortest Path	Minimum Distance
+    [0, 3]	0 → 3	1
+    [2, 4]	2 → 4	1
+    Thus, the output is [1, 1].
+ */
+
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        int LOG = 18;
+
+        vector<pair<int, int>> vec;
+        for (int i = 0; i < n; i++) {
+            vec.push_back({nums[i], i});
+        }
+
+        sort(vec.begin(), vec.end());
+
+        vector<int> getSortIdx(n);
+        for (int i = 0; i < n; i++) {
+            getSortIdx[vec[i].second] = i;
+        }
+
+        vector<vector<int>> st(n, vector<int>(LOG, 0));
+        int l = 0;
+
+        for (int r = 0; r < n; r++) {
+            while (vec[r].first - vec[l].first > maxDiff) {
+                st[l][0] = r - 1;
+                l++;
+            }
+        }
+
+        while (l < n) {
+            st[l][0] = n - 1;
+            l++;
+        }
+
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 0; i < n; i++) {
+                st[i][j] = st[st[i][j - 1]][j - 1];
+            }
+        }
+
+        vector<int> ans(queries.size(), -1);
+
+        for (int i = 0; i < queries.size(); i++) {
+            int a = getSortIdx[queries[i][0]];
+            int b = getSortIdx[queries[i][1]];
+
+            if (a > b) swap(a, b);
+
+            if (a == b) {
+                ans[i] = 0;
+                continue;
+            }
+
+            int cur = a;
+            int step = 0;
+
+            for (int j = LOG - 1; j >= 0; j--) {
+                if (st[cur][j] < b) {
+                    step += 1 << j;
+                    cur = st[cur][j];
+                }
+            }
+
+            if (st[cur][0] >= b) {
+                ans[i] = step + 1;
+            }
+        }
+
+        return ans;
+    }
+};
