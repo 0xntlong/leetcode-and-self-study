7818117b9bd3a622d0a499a4acca93854eb2b959
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-11 12:12:21 +0700

    Update 2685.py

--- python3/medium-solution/2685.py
+++ python3/medium-solution/2685.py
@@ -35,3 +35,37 @@ class Solution:
             if all(len(X[j]) == len(bfs) - 1 for j in bfs):
                 res += 1
         return res
+
+
+
+"""
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range (n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        visited = [False] * n
+        def BFS(node):
+            visited[node] = True
+            queue = deque([node])
+            node, edge = 0, 0
+            while queue:
+                curr = queue.popleft()
+                node += 1
+                edge += len(adj[curr])
+
+                for nei in adj[curr]:
+                    if visited[nei]:
+                        continue
+                    visited[nei] = True
+                    queue.append(nei)
+            edge //= 2
+            return edge == (node) * (node - 1) // 2
+        res = 0
+        for i in range(n):
+            if not visited[i]:
+                res += BFS(i)
+        return res
+"""
