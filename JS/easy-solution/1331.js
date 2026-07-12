261e27d527651f9321f7d3e0618082367ced7ba8
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-12 14:01:17 +0700

    Create 1331.js

---
+++ JS/easy-solution/1331.js
@@ -0,0 +1,35 @@
+/**
+1331. Rank Transform of an Array
+    Given an array of integers arr, replace each element with its rank.
+    The rank represents how large the element is. The rank has the following rules:
+    Rank is an integer starting from 1.
+    The larger the element, the larger the rank. If two elements are equal, their rank must be the same.
+    Rank should be as small as possible.
+
+    Example 
+    Input: arr = [40,10,20,30]
+    Output: [4,1,2,3]
+    Explanation: 40 is the largest element. 10 is the smallest. 20 is the second smallest. 30 is the third smallest.
+ */
+
+
+
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
+    const rankMap = new Map();
+    const sortedArray = [...arr].sort((a, b) => a - b);
+
+    let rank = 1;
+
+    for (const x of sortedArray) {
+        if (!rankMap.has(x)) {
+            rankMap.set(x, rank);
+            rank++;
+        }
+    }
+
+    return arr.map(x => rankMap.get(x));
+};
