f9f2bbccd6b123f3f5f5943a11fb86e0851a38a8
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-13 11:07:08 +0700

    Create 1291.js

---
+++ JS/medium-solution/1291.js
@@ -0,0 +1,36 @@
+/**
+1291. Sequential Digits
+    An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+    Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+    Example :
+    Input: low = 100, high = 300
+    Output: [123,234]
+ */
+
+
+
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+var sequentialDigits = function(low, high) {
+    const digits = "123456789";
+    const res = [];
+
+    const n = String(low).length;
+    const m = String(high).length;
+
+    for (let length = n; length <= m; length++) {
+        for (let i = 0; i < 10 - length; i++) {
+            const num = Number(digits.slice(i, i + length));
+
+            if (low <= num && num <= high) {
+                res.push(num);
+            }
+        }
+    }
+
+    return res;
+};
