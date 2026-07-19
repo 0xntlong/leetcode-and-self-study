2be951f6b7c15c2ad4dfc67ca20a181a63bfe957
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-19 13:49:09 +0700

    Create 1081.cc

---
+++ cpp/medium-solution/1081.cc
@@ -0,0 +1,8 @@
+/**
+1081. Smallest Subsequence of Distinct Characters
+    Given a string s, return the lexicographically smallest subsequence of s that contains all the distinct characters of s exactly once.
+
+    Example :
+    Input: s = "bcabc"
+    Output: "abc"
+ */
