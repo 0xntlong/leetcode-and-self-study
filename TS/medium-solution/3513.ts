2655329f1bf234d5bcb471ef2c2fd7e956c31e43
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-23 14:42:31 +0700

    Create 3513.ts

---
+++ TS/medium-solution/3513.ts
@@ -0,0 +1,30 @@
+/**
+3513. Number of Unique XOR Triplets I
+    You are given an integer array nums of length n, where nums is a permutation of the numbers in the range [1, n].
+    A XOR triplet is defined as the XOR of three elements nums[i] XOR nums[j] XOR nums[k] where i <= j <= k.
+    Return the number of unique XOR triplet values from all possible triplets (i, j, k).
+
+    Example 1:
+    Input: nums = [1,2]
+    Output: 2
+    Explanation:
+    The possible XOR triplet values are:
+    (0, 0, 0) → 1 XOR 1 XOR 1 = 1
+    (0, 0, 1) → 1 XOR 1 XOR 2 = 2
+    (0, 1, 1) → 1 XOR 2 XOR 2 = 1
+    (1, 1, 1) → 2 XOR 2 XOR 2 = 2
+    The unique XOR values are {1, 2}, so the output is 2.
+ */
+
+
+function uniqueXorTriplets(nums: number[]): number {
+    const n = nums.length;
+
+    if (n < 3) {
+        return n;
+    }
+
+    const bits = Math.floor(Math.log2(n)) + 1;
+
+    return 1 << bits;
+};
