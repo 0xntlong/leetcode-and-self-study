2b1d2ac71423358925f76a8838febbb9f15ff8d5
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-07-25 14:51:16 +0700

    Create 3536.js

---
+++ JS/easy-solution/3536.js
@@ -0,0 +1,30 @@
+/**
+3536. Maximum Product of Two Digits
+    You are given a positive integer n.
+    Return the maximum product of any two digits in n.
+    Note: You may use the same digit twice if it appears more than once in n.
+
+
+    Example :
+    Input: n = 31
+    Output: 3
+    Explanation:
+
+    The digits of n are [3, 1].
+    The possible products of any two digits are: 3 * 1 = 3.
+    The maximum product is 3.
+ */
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var maxProduct = function(n) {
+    const s = String(n).split('').sort();
+
+    const d1 = Number(s[s.length - 2]);
+    const d2 = Number(s[s.length - 1]);
+
+    return d1 * d2;
+};
