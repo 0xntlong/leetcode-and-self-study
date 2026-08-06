798f5952e20d8752a92b797dd2f9cf332a076d5d
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-08-06 15:19:57 +0700

    Create 3345.cc

---
+++ cpp/easy-solution/3345.cc
@@ -0,0 +1,32 @@
+/**
+3345. Smallest Divisible Digit Product I
+    You are given two integers n and t. Return the smallest number greater than or equal to n such that the product of its digits is divisible by t.
+    Example :
+    Input: n = 10, t = 2
+    Output: 10
+    Explanation:
+    The digit product of 10 is 0, which is divisible by 2, making it the smallest number greater than or equal to 10 that satisfies the condition.
+ */
+
+
+
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        for (int i = n; i < n + 1000; i++) {
+            int x = i;
+            int product = 1;
+
+            while (x > 0) {
+                product *= x % 10;
+                x /= 10;
+            }
+
+            if (product % t == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
