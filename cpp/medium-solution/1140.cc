22434a025c141ea231799c3546dc8e7c63f5b5a5
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-08-09 14:00:33 +0700

    Create 1140.cc

---
+++ cpp/medium-solution/1140.cc
@@ -0,0 +1,52 @@
+/**
+1140. Stone Game II
+    Alice and Bob continue their games with piles of stones. There are a number of piles arranged in a row, and each pile has a positive integer number of stones piles[i]. The objective of the game is to end with the most stones.
+    Alice and Bob take turns, with Alice starting first.
+    On each player's turn, that player can take all the stones in the first X remaining piles, where 1 <= X <= 2M. Then, we set M = max(M, X). Initially, M = 1.
+    The game continues until all the stones have been taken.
+    Assuming Alice and Bob play optimally, return the maximum number of stones Alice can get.
+    Example :
+    Input: piles = [2,7,9,4,4]
+    Output: 10
+    Explanation:
+    If Alice takes one pile at the beginning, Bob takes two piles, then Alice takes 2 piles again. Alice can get 2 + 4 + 4 = 10 stones in total.
+    If Alice takes two piles at the beginning, then Bob can take all three piles left. In this case, Alice get 2 + 7 = 9 stones in total.
+    So we return 10 since it's larger.
+*/
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+
+        vector<vector<int>> dp(n, vector<int>(n + 1, 0));
+        vector<int> suffixSum(n, 0);
+
+        suffixSum[n - 1] = piles[n - 1];
+
+        for (int x = n - 2; x >= 0; x--) {
+            suffixSum[x] = suffixSum[x + 1] + piles[x];
+        }
+
+        for (int x = n - 1; x >= 0; x--) {
+            for (int y = 1; y <= n; y++) {
+                if (x + 2 * y >= n) {
+                    dp[x][y] = suffixSum[x];
+                } else {
+                    for (int j = 1; j <= 2 * y; j++) {
+                        dp[x][y] = max(
+                            dp[x][y],
+                            suffixSum[x] - dp[x + j][max(y, j)]
+                        );
+                    }
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
+};
