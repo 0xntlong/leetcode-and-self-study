97319ca3f798e0036eec5793a6874542d91c2e39
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-08-12 08:22:04 +0700

    Create 2958.py

---
+++ python3/medium-solution/2958.py
@@ -0,0 +1,31 @@
+"""
+2958. Length of Longest Subarray With at Most K Frequency
+    You are given an integer array nums and an integer k.
+    The frequency of an element x is the number of times it occurs in an array.
+    An array is called good if the frequency of each element in this array is less than or equal to k.
+    Return the length of the longest good subarray of nums.
+    A subarray is a contiguous non-empty sequence of elements within an array.
+
+    Example :
+    Input: nums = [1,2,3,1,2,3,1,2], k = 2
+    Output: 6
+    Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good.
+    It can be shown that there are no good subarrays with length more than 6.
+"""
+
+
+
+from typing import List
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        left = 0
+        res = 0
+        count = {}
+        n = len(nums)
+        for i in range(n):
+            count[nums[i]] = count.get(nums[i], 0) + 1
+            while count[nums[i]] > k:
+                count[nums[left]] -= 1
+                left += 1
+            res = max(res, i - left + 1)
+        return res
