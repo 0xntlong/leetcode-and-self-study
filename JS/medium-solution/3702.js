cb61046e3727e2ebf55c31081ae1f1676144a8a1
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-08-15 09:21:21 +0700

    Create 3702.js

---
+++ JS/medium-solution/3702.js
@@ -0,0 +1,32 @@
+/**
+3702. Longest Subsequence With Non-Zero Bitwise XOR
+    You are given an integer array nums.
+    Return the length of the longest subsequence in nums whose bitwise XOR is non-zero. If no such subsequence exists, return 0.
+    Example :
+    Input: nums = [1,2,3]
+    Output: 2
+    Explanation:
+    One longest subsequence is [2, 3]. The bitwise XOR is computed as 2 XOR 3 = 1, which is non-zero.
+ */
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubsequence = function(nums) {
+    const n = nums.length;
+
+    if (nums.every(num => num === 0)) {
+        return 0;
+    }
+
+    let x = 0;
+
+    for (const num of nums) {
+        x ^= num;
+    }
+
+    return x ? n : n - 1;
+};
