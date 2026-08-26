a71ca74f9212bdc9ed00ebc9bc6ae5abda9e8497
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-08-26 10:45:01 +0700

    Create 2904.js

---
+++ JS/medium-solution/2904.js
@@ -0,0 +1,81 @@
+/**
+2904. Shortest and Lexicographically Smallest Beautiful String
+    You are given a binary string s and a positive integer k.
+    A substring of s is beautiful if the number of 1's in it is exactly k.
+    Let len be the length of the shortest beautiful substring.
+    Return the lexicographically smallest beautiful substring of string s with length equal to len. If s doesn't contain a beautiful substring, return an empty string.
+    A string a is lexicographically larger than a string b (of the same length) if in the first position where a and b differ, a has a character strictly larger than the corresponding character in b.
+    For example, "abcd" is lexicographically larger than "abcc" because the first position they differ is at the fourth character, and d is greater than c.
+    Example :
+    Input: s = "100011001", k = 3
+    Output: "11001"
+    Explanation: There are 7 beautiful substrings in this example:
+    1. The substring "100011001".
+    2. The substring "100011001".
+    3. The substring "100011001".
+    4. The substring "100011001".
+    5. The substring "100011001".
+    6. The substring "100011001".
+    7. The substring "100011001".
+    The length of the shortest beautiful substring is 5.
+    The lexicographically smallest beautiful substring with length 5 is the substring "11001".
+ */
+
+
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var shortestBeautifulSubstring = function(s, k) {
+    let totalOnes = 0;
+
+    for (const ch of s) {
+        if (ch === '1') {
+            totalOnes++;
+        }
+    }
+
+    if (totalOnes < k) {
+        return "";
+    }
+
+    let left = 0;
+    let countOne = 0;
+    let res = "";
+
+    for (let right = 0; right < s.length; right++) {
+        if (s[right] === '1') {
+            countOne++;
+        }
+
+        while (
+            countOne > k ||
+            (left <= right && s[left] === '0')
+        ) {
+            if (s[left] === '1') {
+                countOne--;
+            }
+
+            left++;
+        }
+
+        if (countOne === k) {
+            const current = s.slice(left, right + 1);
+
+            if (
+                res === "" ||
+                current.length < res.length ||
+                (
+                    current.length === res.length &&
+                    current < res
+                )
+            ) {
+                res = current;
+            }
+        }
+    }
+
+    return res;
+};
