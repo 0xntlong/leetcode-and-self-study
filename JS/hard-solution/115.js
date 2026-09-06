1338dd67f95a60005223845de42dfa84b3be0057
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-09-06 14:35:24 +0700

    Create 115.js

---
+++ JS/hard-solution/115.js
@@ -0,0 +1,38 @@
+/**
+115. Distinct Subsequences
+    Given two strings s and t, return the number of distinct subsequences of s which equals t.
+    The test cases are generated so that the answer fits on a 32-bit signed integer.
+    Example :
+    Input: s = "rabbbit", t = "rabbit"
+    Output: 3
+    Explanation:
+    As shown below, there are 3 ways you can generate "rabbit" from s.
+    rabbbit
+    rabbbit
+    rabbbit
+ */
+
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var numDistinct = function(s, t) {
+    const m = s.length;
+    const n = t.length;
+
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = Math.min(i, n); j >= 1; j--) {
+            if (s[i - 1] === t[j - 1]) {
+                dp[j] += dp[j - 1];
+            }
+        }
+    }
+
+    return dp[n];
+};
