abe00e49218c44287be4b688a82be5b56a94760c
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-09-08 14:32:06 +0700

    Create 3870.js

---
+++ JS/easy-solution/3870.js
@@ -0,0 +1,32 @@
+/**
+3870. Count Commas in Range
+    You are given an integer n.
+    Return the total number of commas used when writing all integers from [1, n] (inclusive) in standard number formatting.
+    In standard formatting:
+    A comma is inserted after every three digits from the right.
+    Numbers with fewer than 4 digits contain no commas.
+    Example :
+    Input: n = 1002
+    Output: 3
+    Explanation:
+    The numbers "1,000", "1,001", and "1,002" each contain one comma, giving a total of 3.
+ */
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countCommas = function(n) {
+    if (n < 1000) {
+        return 0;
+    }
+
+    const len = String(n).length;
+
+    if (len % 3 === 0) {
+        return (len / 3 - 1) + (n - 1000);
+    }
+
+    return Math.floor(len / 3) + n - 1000;
+};
