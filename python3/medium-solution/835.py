3c336e0abf0c80c9fb62096aab347bcd4e7be4aa
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-09-13 16:28:41 +0700

    Create 835.py

---
+++ python3/medium-solution/835.py
@@ -0,0 +1,34 @@
+"""
+835. Image Overlap
+    You are given two images, img1 and img2, represented as binary, square matrices of size n x n. A binary matrix has only 0s and 1s as values.
+    We translate one image however we choose by sliding all the 1 bits left, right, up, and/or down any number of units. We then place it on top of the other image. We can then calculate the overlap by counting the number of positions that have a 1 in both images.
+    Note also that a translation does not include any kind of rotation. Any 1 bits that are translated outside of the matrix borders are erased.
+    Return the largest possible overlap.
+
+    Example :
+    Input: img1 = [[1,1,0],[0,1,0],[0,1,0]], img2 = [[0,0,0],[0,1,1],[0,0,1]]
+    Output: 3
+    Explanation: We translate img1 to right by 1 unit and down by 1 unit.
+    The number of positions that have a 1 in both images is 3 (shown in red).
+"""
+from typing import List
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        n = len(img1)
+        result = 0
+
+        for dx in range(-n + 1, n):
+            for dy in range(-n + 1, n):
+                score = 0
+
+                for i in range(n):
+                    for j in range(n):
+                        new_i = i + dx
+                        new_j = j + dy
+
+                        if 0 <= new_i < n and 0 <= new_j < n:
+                            score += img1[new_i][new_j] & img2[i][j]
+
+                result = max(result, score)
+
+        return result
