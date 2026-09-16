587d9ebf643258c7598953c8f1c93380fdfa0612
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-09-16 15:15:19 +0700

    Create 1621.ts

---
+++ TS/medium-solution/1621.ts
@@ -0,0 +1,40 @@
+/**
+1621. Number of Sets of K Non-Overlapping Line Segments
+    Given n points on a 1-D plane, where the ith point (from 0 to n-1) is at x = i, find the number of ways we can draw exactly k non-overlapping line segments such that each segment covers two or more points. The endpoints of each segment must have integral coordinates. The k line segments do not have to cover all n points, and they are allowed to share endpoints.
+    Return the number of ways we can draw k non-overlapping line segments. Since this number can be huge, return it modulo 109 + 7.
+
+    Example :
+    Input: n = 4, k = 2
+    Output: 5
+    Explanation: The two line segments are shown in red and blue.
+    The image above shows the 5 different ways {(0,2),(2,3)}, {(0,1),(1,3)}, {(0,1),(2,3)}, {(1,2),(2,3)}, {(0,1),(1,2)}.
+ */
+
+
+
+function numberOfSets(n: number, k: number): number {
+    const MOD = 1000000007;
+
+    const dp: number[] = new Array(n).fill(1);
+    const prefixSums: number[] = new Array(n + 1).fill(0);
+
+    for (let j = 0; j < n; j++) {
+        prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+    }
+
+    for (let i = 0; i < k; i++) {
+        dp[0] = 0;
+
+        for (let j = 1; j < n; j++) {
+            dp[j] = (dp[j - 1] + prefixSums[j]) % MOD;
+        }
+
+        prefixSums[0] = 0;
+
+        for (let j = 0; j < n; j++) {
+            prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+        }
+    }
+
+    return dp[n - 1];
+};
