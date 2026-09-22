55293a9008d8818e946c81439a70badb3d95bfc5
Andrew Nguyen <155884683+0xntlong@users.noreply.github.com>
2026-09-22 14:55:02 +0700

    Update 3525.ts

--- TS/hard-solution/3525.ts
+++ TS/hard-solution/3525.ts
@@ -1,79 +1,29 @@
 /**
-3625. Count Number of Trapezoids II
-    You are given a 2D integer array points where points[i] = [xi, yi] represents the coordinates of the ith point on the Cartesian plane.
-    Return the number of unique trapezoids that can be formed by choosing any four distinct points from points.
-    A trapezoid is a convex quadrilateral with at least one pair of parallel sides. Two lines are parallel if and only if they have the same slope.
+3525. Find X Value of Array II
+    You are given an array of positive integers nums and a positive integer k. You are also given a 2D array queries, where queries[i] = [indexi, valuei, starti, xi].
+    You are allowed to perform an operation once on nums, where you can remove any suffix from nums such that nums remains non-empty.
+    The x-value of nums for a given x is defined as the number of ways to perform this operation so that the product of the remaining elements leaves a remainder of x modulo k.
+    For each query in queries you need to determine the x-value of nums for xi after performing the following actions:
+    Update nums[indexi] to valuei. Only this step persists for the rest of the queries.
+    Remove the prefix nums[0..(starti - 1)] (where nums[0..(-1)] will be used to represent the empty prefix).
+    Return an array result of size queries.length where result[i] is the answer for the ith query.
+    A prefix of an array is a subarray that starts from the beginning of the array and extends to any point within it.
+    A suffix of an array is a subarray that starts at any point within the array and extends to the end of the array.
+    Note that the prefix and suffix to be chosen for the operation can be empty.
+    Note that x-value has a different definition in this version.
 
-    Example 1:
-    Input: points = [[-3,2],[3,0],[2,3],[3,2],[2,-3]]
-    Output: 2
+    Example :
+
+    Input: nums = [1,2,3,4,5], k = 3, queries = [[2,2,0,2],[3,3,3,0],[0,1,0,1]]
+    Output: [2,2,2]
     Explanation:
-    There are two distinct ways to pick four points that form a trapezoid:
-
-    The points [-3,2], [2,3], [3,2], [2,-3] form one trapezoid.
-    The points [2,3], [3,2], [3,0], [2,-3] form another trapezoid.
+    For query 0, nums becomes [1, 2, 2, 4, 5], and the empty prefix must be removed. The possible operations are:
+    Remove the suffix [2, 4, 5]. nums becomes [1, 2].
+    Remove the empty suffix. nums becomes [1, 2, 2, 4, 5] with a product 80, which gives remainder 2 when divided by 3.
+    For query 1, nums becomes [1, 2, 2, 3, 5], and the prefix [1, 2, 2] must be removed. The possible operations are:
+    Remove the empty suffix. nums becomes [3, 5].
+    Remove the suffix [5]. nums becomes [3].
+    For query 2, nums becomes [1, 2, 2, 3, 5], and the empty prefix must be removed. The possible operations are:
+    Remove the suffix [2, 2, 3, 5]. nums becomes [1].
+    Remove the suffix [3, 5]. nums becomes [1, 2, 2].
  */
-
-
-
-function gcd(a: number, b: number): number {
-    a = Math.abs(a);
-    b = Math.abs(b);
-    while (b !== 0) {
-        let tmp = a % b;
-        a = b;
-        b = tmp;
-    }
-    return a;
-}
-
-function count(map: Map<any, Map<any, number>>): number {
-    let ans = 0;
-
-    for (let inner of map.values()) {
-        let total = 0;
-        for (let val of inner.values()) total += val;
-
-        let rem = total;
-        for (let val of inner.values()) {
-            rem -= val;
-            ans += val * rem;
-        }
-    }
-    return ans;
-}
-
-export var countTrapezoids = function(points: number[][]): number {
-    const slopes = new Map<number, Map<number, number>>();
-    const lines = new Map<number, Map<number, number>>();
-    const n = points.length;
-
-    function add(map: Map<number, Map<number, number>>, key: number, des: number): void {
-        if (!map.has(key)) map.set(key, new Map());
-        const inner = map.get(key);
-        inner!.set(des, (inner!.get(des) || 0) + 1);
-    }
-
-    for (let i = 0; i < n; i++) {
-        let [x1, y1] = points[i];
-        for (let j = i + 1; j < n; j++) {
-            let [x2, y2] = points[j];
-            let dx = x2 - x1;
-            let dy = y2 - y1;
-            if (dx < 0 || (dx === 0 && dy < 0)) {
-                dx = -dx;
-                dy = -dy;
-            }
-            let g = gcd(dx, Math.abs(dy));
-            let x = dx / g;
-            let y = dy / g;
-            let inter = x * y1 - y * x1;
-            let key1 = (x << 12) | (y + 2000);
-            let key2 = (dx << 12) | (dy + 2000);
-            add(slopes, key1, inter);
-            add(lines, key2, inter);
-        }
-    }
-
-    return count(slopes) - Math.floor(count(lines) / 2);
-};
